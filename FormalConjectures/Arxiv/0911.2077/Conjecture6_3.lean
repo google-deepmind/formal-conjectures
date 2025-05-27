3da519f5e442df3eafd30d2c34f2241e5c61d262
Calle Sonne <calle.sonne@gmail.com>
2025-05-27 13:57:24 +0000

    fix copy-paste mistake

--- FormalConjectures/Arxiv/0911.2077/Conjecture6_3.lean
+++ FormalConjectures/Arxiv/0911.2077/Conjecture6_3.lean
@@ -47,4 +47,4 @@ theorem arxiv.id0911_2077.conjecture6_3
     le_trans (le_of_lt (Set.mem_Ioo.mp h_p).right) (by linarith)
   ((PMF.binomial (.ofReal p : ℝ≥0∞) hp' (2 * k)).toMeasure (Set.Ici k)).toReal
  ≥ 1 - Φ ((1 / 2 - p) * sqrt (2 * k : ℝ≥0) / σ)
-    + ((2 * k).choose k) * σ.rpow (2 * k) := sorry
+    + (1 / 2) * ((2 * k).choose k) * σ ^ (2 * k) := sorry
