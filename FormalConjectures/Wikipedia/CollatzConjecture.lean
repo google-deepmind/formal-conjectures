7f07421bf3143825fa21938440978b0e86b398f7
exekis <kiarashsotoudeh@gmail.com>
2025-05-28 15:50:00 -0400

    formalize Collatz conjecture (issue #83)

---
+++ FormalConjectures/Wikipedia/CollatzConjecture.lean
@@ -0,0 +1,35 @@
+/-
+Copyright 2025 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+import FormalConjectures.Util.ProblemImports
+
+/-!
+# Collatz Conjecture
+
+*Reference:* [Wikipedia](https://en.wikipedia.org/wiki/Collatz_conjecture)
+-/
+
+/--
+The Collatz step => if `n` is even, halve it, otherwise multiply by three and add one.
+-/
+def collatz_fn : ℕ → ℕ
+| n =>
+  match n % 2 with
+  | 0   => n / 2
+  | _   => 3 * n + 1
+
+@[category research open, AMS 11]
+theorem collatz_conjecture (n : ℕ) : ∃ m, Nat.iterate collatz_fn m n = 1 := sorry
