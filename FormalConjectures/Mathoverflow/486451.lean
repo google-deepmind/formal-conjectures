97b54b3b40842a1b617c081da36ebf72251da872
Junyan Xu <junyanxu.math@gmail.com>
2025-05-29 15:21:19 +0200

    switch to answer(sorry)

--- FormalConjectures/Mathoverflow/486451.lean
+++ FormalConjectures/Mathoverflow/486451.lean
@@ -22,18 +22,18 @@ import FormalConjectures.Util.ProblemImports
 asked by user [*Junyan Xu*](https://mathoverflow.net/users/3332/junyan-xu)
 -/
 
-@[category research solved, AMS 16]
 /-- There exists a semiring with a unique left maximal ideal but more than one right maximal ideals. -/
+@[category research solved, AMS 16]
 theorem exists_semiring_unique_left_maximal_not_unique_right_maximal :
     ∃ (R : Type) (_ : Semiring R), (∃! I : Ideal R, I.IsMaximal) ∧
       ∃ I J : Ideal Rᵐᵒᵖ, I.IsMaximal ∧ J.IsMaximal ∧ I ≠ J := by
   sorry
 
-@[category research open, AMS 16]
 /-- There exists a semiring with a unique left maximal ideal and a unique right maximal ideal
 which are not the same as sets. -/
+@[category research open, AMS 16]
 theorem exists_semiring_unique_left_right_maximal_ne :
-    ∃ (R : Type) (_ : Semiring R), (∃! I : Ideal R, I.IsMaximal) ∧
-      (∃! J : Ideal Rᵐᵒᵖ, J.IsMaximal) ∧
-      ∃ (I : Ideal R) (J : Ideal Rᵐᵒᵖ), (I : Set R) ≠ MulOpposite.op ⁻¹' J := by
+    (∃ (R : Type) (_ : Semiring R),
+      (∃! I : Ideal R, I.IsMaximal) ∧ (∃! J : Ideal Rᵐᵒᵖ, J.IsMaximal) ∧
+      ∃ (I : Ideal R) (J : Ideal Rᵐᵒᵖ), (I : Set R) ≠ MulOpposite.op ⁻¹' J) ↔ answer(sorry) := by
   sorry
