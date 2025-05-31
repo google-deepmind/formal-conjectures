268d892ecbb6deaecabfb8f208395f99fc712e5f
Seewoo Lee <seewoo5@gmail.com>
2025-05-31 00:58:48 -0700

    ramanujan-petersson

---
+++ FormalConjectures/Wikipedia/RamanujanPetersson.lean
@@ -0,0 +1,34 @@
+/-
+Copyright 2025 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+import FormalConjectures.Util.ProblemImports
+
+/-!
+# Ramanujan-Petersson Conjecture
+
+*Reference:* [Wikipedia](https://en.wikipedia.org/wiki/Ramanujan%E2%80%93Petersson_conjecture)
+-/
+
+open PowerSeries PowerSeries.WithPiTopology
+
+noncomputable def Δ : PowerSeries ℝ := X * ∏' (n : ℕ+), (1 - X ^ (n : ℕ)) ^ 24
+
+noncomputable def τ (n : ℕ) : ℝ := PowerSeries.coeff ℝ n Δ
+
+
+@[category research solved, AMS 11]
+theorem ramanujan_petersson : ∀ p : ℕ, Prime p → abs (τ p) ≤ 2 * p ^ ((11 : ℝ) / 2) := by
+  sorry
