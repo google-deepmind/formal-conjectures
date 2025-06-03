52a3e1519dec38bc031c6be5ffee9ea38f19cd79
Hiroki Ninomiya <57189658+ninohiro@users.noreply.github.com>
2025-06-04 00:05:15 +0900

    update

--- FormalConjectures/Wikipedia/LehmerMahlerMeasureProblem.lean
+++ FormalConjectures/Wikipedia/LehmerMahlerMeasureProblem.lean
@@ -22,7 +22,6 @@ import FormalConjectures.Util.ProblemImports
 *Reference:* [Wikipedia](https://en.wikipedia.org/wiki/Lehmer%27s_conjecture)
 -/
 
-open Multiset
 open Polynomial
 
 noncomputable section
@@ -32,13 +31,10 @@ The Mahler measure of `f(X)` is defined as `‖a‖ ∏ᵢ max(1,‖αᵢ‖)`,
 where `f(X)=a(X-α₁)(X-α₂)...(X-αₙ)`.
 -/
 def mahlerMeasure (f : ℂ[X]) : ℝ :=
-  ‖f.leadingCoeff‖ * (map (max 1 ‖·‖) f.roots).prod
-
-def toC (f : ℤ[X]) : ℂ[X] :=
-  f.map (algebraMap ℤ ℂ)
+  ‖f.leadingCoeff‖ * (f.roots.map (max 1 ‖·‖)).prod
 
 def mahlerMeasureZ (f : ℤ[X]) : ℝ :=
-  mahlerMeasure (toC f)
+  mahlerMeasure (f.map (algebraMap ℤ ℂ))
 
 /--
 Let `M(f)` denote the Mahler measure of `f`.
@@ -47,7 +43,7 @@ There exists a constant `μ>1` such that for any `f(x)∈ℤ[x], M(f)>1 → M(f)
 @[category research open, AMS 11]
 theorem lehmer_mahler_measure_problem :
     ∃ μ : ℝ, ∀ f : ℤ[X],
-    μ > 1 ∧ (mahlerMeasureZ f > 1 → mahlerMeasureZ f ≥ μ) := by
+      μ > 1 ∧ (mahlerMeasureZ f > 1 → mahlerMeasureZ f ≥ μ) := by
   sorry
 
 def lehmerPolynomial : ℤ[X] := X^10 + X^9 - X^7 - X^6 - X^5 - X^4 - X^3 + X + 1
@@ -56,21 +52,16 @@ def lehmerPolynomial : ℤ[X] := X^10 + X^9 - X^7 - X^6 - X^5 - X^4 - X^3 + X + 
 `μ=M(X^10 + X^9 - X^7 - X^6 - X^5 - X^4 - X^3 + X + 1)` is the best value for `lehmer_mahler_measure_problem`.
 -/
 @[category research open, AMS 11]
-theorem lehmer_mahler_measure_problem.variants.best :
-    ∀ f : ℤ[X],
-    mahlerMeasureZ f > 1 → mahlerMeasureZ f ≥ mahlerMeasureZ lehmerPolynomial := by
+theorem lehmer_mahler_measure_problem.variants.best (f : ℤ[X])
+    (hf : mahlerMeasureZ f > 1) : mahlerMeasureZ f ≥ mahlerMeasureZ lehmerPolynomial := by
   sorry
 
-def IsReciprocal (f : ℤ[X]) : Prop :=
-  f.reverse = f
-
 /--
-If `f` is not reciprocal, `M(f) ≥ M(X^3 - X - 1)`.
+If `f` is not reciprocal and `M(f)>1` then `M(f) ≥ M(X^3 - X - 1)`.
 -/
 @[category research solved, AMS 11]
-theorem lehmer_mahler_measure_problem.variants.not_reciprocal :
-    ∀ f : ℤ[X],
-    ¬ IsReciprocal f ∧ mahlerMeasureZ f > 1 →
+theorem lehmer_mahler_measure_problem.variants.not_reciprocal (f : ℤ[X])
+    (hf : mahlerMeasureZ f > 1) (hf' : f.reverse ≠ f) :
     mahlerMeasureZ f ≥ mahlerMeasureZ (X^3 - X - 1) := by
   sorry
 
@@ -78,11 +69,10 @@ def AllCoeffsOdd (f : Polynomial ℤ) : Prop :=
   ∀ i : ℕ, Odd (f.coeff i)
 
 /--
-If all the coefficients of `f` are odd, `M(f) ≥ M(X^2 - X - 1)`.
+If all the coefficients of `f` are odd and `M(f)>1`, `M(f) ≥ M(X^2 - X - 1)`.
 -/
 @[category research solved, AMS 11]
-theorem lehmer_mahler_measure_problem.variants.odd :
-    ∀ f : ℤ[X],
-    AllCoeffsOdd f ∧ mahlerMeasureZ f > 1 →
+theorem lehmer_mahler_measure_problem.variants.odd (f : ℤ[X])
+    (hf : mahlerMeasureZ f > 1) (hf' : AllCoeffsOdd f) :
     mahlerMeasureZ f ≥ mahlerMeasureZ (X^2 - X - 1) := by
   sorry
