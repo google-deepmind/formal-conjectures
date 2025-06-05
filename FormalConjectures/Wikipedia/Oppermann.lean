e7eae4fc1a641502747346e4cc09c362de4e3897
Paul Lezeau <lezeau@google.com>
2025-06-05 13:59:07 +0000

    Add implications to other conjectures

--- FormalConjectures/Wikipedia/Oppermann.lean
+++ FormalConjectures/Wikipedia/Oppermann.lean
@@ -37,3 +37,32 @@ For every integer `x ≥ 2` there exists a prime between `x²` and `x(x+1)`.
 theorem oppermann_conjecture.parts.ii (x : ℕ) (hx : x ≥ 2) :
   ∃ p ∈ Finset.Ioo (x^2) (x * (x + 1)), Prime p := by
   sorry
+
+/--
+**Oppermann's Conjecture**:
+For every integer `x ≥ 2`, the following hold:
+- There exists a prime between `x * (x-1)` and `x ^ 2`.
+- There exists a prime between `x ^ 2` and `x * (x+1)`.
+-/
+@[category research open, AMS 11]
+theorem oppermann_conjecture (x : ℕ) (hx : x ≥ 2) :
+  (∃ p ∈ Finset.Ioo (x * (x - 1)) (x^2), Prime p) ∧
+    ∃ p ∈ Finset.Ioo (x^2) (x * (x + 1)), Prime p := by
+  sorry
+
+/-- Oppermann's conjecture implies Brocard's conjecture. -/
+@[category high_school, AMS 11]
+theorem oppermann_implies_brocard (n : ℕ)
+    (P : type_of% oppermann_conjecture) :
+    letI prev := n.nth Prime
+    letI next := (n+1).nth Prime
+    letI primesInBetween := (Finset.Ioo (prev ^ 2) (next ^ 2)).filter Nat.Prime
+    4 ≤ primesInBetween.card := by
+  sorry
+
+/-- Oppermann's conjecture implies Legendre's conjecture. -/
+@[category high_school, AMS 11]
+theorem oppermann_implies_legendre (n : ℕ) (hn : 1 ≤ n)
+    (P : type_of% oppermann_conjecture) :
+    ∃ p ∈ Set.Ioo (n ^ 2) ((n + 1) ^ 2), Prime p := by
+  sorry
