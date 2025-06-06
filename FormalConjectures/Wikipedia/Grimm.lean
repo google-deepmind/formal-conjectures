da0bb546eaadc8482125d165a6a07798d366cd1f
Reklle <53151326+Reklle@users.noreply.github.com>
2025-06-06 15:41:46 +0300

    Update FormalConjectures/Wikipedia/Grimm.lean
    Co-authored-by: Junyan Xu <junyanxu.math@gmail.com>

--- FormalConjectures/Wikipedia/Grimm.lean
+++ FormalConjectures/Wikipedia/Grimm.lean
@@ -28,8 +28,8 @@ If $n, n+1, \dots, n+k-1$ are all composite numbers,
 then there are $k$ distinct primes $p_i$ such that $p_i$ divides $n + i$ for
 -/
 @[category research open, AMS 11]
-theorem grimm_conjecture (n : ℕ+) (k : ℕ+) (h : ∀ i : Fin k, ¬ (n + i).Prime) :
-    ∃ (ps : Fin k → ℕ), IsSetOfPrimes k ps ∧ (∀ i : Fin k, ps i ∣ (n + i)) := by
+theorem grimm_conjecture (n k : ℕ+) (h : ∀ i : Fin k, ¬ (n + i).Prime) :
+    ∃ ps : Fin k → ℕ, IsSetOfPrimes k ps ∧ ∀ i : Fin k, ps i ∣ (n + i) := by
   sorry
 
 /--
@@ -37,6 +37,6 @@ If $n, n+1, \dots, n+k-1$ are all composite numbers,
 then their product has at least $k$ distinct prime divisors.
 -/
 @[category research open, AMS 11]
-theorem grimm_conjecture_weak (n k : ℕ+) (h: ∀ (i : Fin k), Prime (n + i)) :
-    ∃ (ps : Fin k → ℕ), IsSetOfPrimes k ps ∧ (∀ (i : Fin k), ∃ (j : Fin k), ps i ∣ (n + j)) := by
+theorem grimm_conjecture_weak (n k : ℕ+) (h : ∀ (i : Fin k), ¬ (n + i).Prime) :
+    ∃ ps : Fin k → ℕ, IsSetOfPrimes k ps ∧ ∀ i : Fin k, ∃ j : Fin k, ps i ∣ (n + j) := by
   sorry
