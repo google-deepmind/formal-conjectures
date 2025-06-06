570d32ee15003defc60f4c9e5ac65f21276e10f6
Reklle <53151326+Reklle@users.noreply.github.com>
2025-06-06 12:58:05 +0300

    Add Gilbreath's conjecture

---
+++ FormalConjectures/Wikipedia/Gilbreath.lean
@@ -0,0 +1,37 @@
+/-
+Copyright 2025 The Formal Conjectures Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+import FormalConjectures.Util.ProblemImports
+
+/-!
+# Gilbreath's conjecture
+
+*Reference:* [Wikipedia](https://en.wikipedia.org/wiki/Gilbreath%27s_conjecture)
+-/
+
+/--
+**Gilbreath nth difference**, $d^n$
+Let $d^0(n) = p_n$ and $d^k(n) = |d^{k-1}(n+1) - d^{k-1}(n)|
+-/
+noncomputable def GilbreathNthDerivative : ℕ → (ℕ → ℤ)
+  | 0 => fun n => n.nth Prime
+  | k + 1 => let prev := GilbreathNthDerivative k
+    fun n => (prev (n + 1) - prev n).natAbs
+
+/--
+Gilbreath nth difference sequence begins with 1.
+-/
+@[category research open, AMS 11]
+theorem gilbreath_conjecture (n : ℕ+) :
+    GilbreathNthDerivative n 0 = 1 := by
+  sorry
