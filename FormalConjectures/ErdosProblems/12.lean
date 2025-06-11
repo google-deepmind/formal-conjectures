215ef8a66bbb29c48935cdb305a921b79c9dbdf3
Paul Lezeau <lezeau@google.com>
2025-06-11 12:35:10 +0000

    Review comments

--- FormalConjectures/ErdosProblems/12.lean
+++ FormalConjectures/ErdosProblems/12.lean
@@ -28,14 +28,18 @@ open Classical
 
 /--
 A set `A` is "good" if it is infinite and there are no distinct `a,b,c` in `A`
-such that `a∣(b+c)` and `b > a`, `c > a`.
+such that `a ∣ (b+c)` and `b > a`, `c > a`.
 -/
 abbrev IsGood (A : Set ℕ) : Prop := A.Infinite ∧
   ∀ᵉ (a ∈ A) (b ∈ A) (c ∈ A), a ∣ b + c → a < b →
   a < c → b = c
 
+/-- Given a set of natural numbers `A`, `Set.bdd A N` is the set `{1,...,N} ∩ A`-/
+private noncomputable def Set.bdd (A : Set ℕ) (N : ℕ) : Finset ℕ :=
+    Finset.Icc 1 N |>.filter (· ∈ A)
+
 /-- The set of $p ^ 2$ where $p \cong 3 \mod 4$ is prime is an example of a good set. -/
-@[category test, AMS 11]
+@[category undergraduate, AMS 11]
 theorem isGood_example :
     IsGood {p ^ 2 | (p : ℕ) (_ : p ≡ 3 [MOD 4]) (_ : p.Prime)} := by
   sorry
@@ -48,7 +52,7 @@ $\liminf \frac{|A \cap \{1, \dotsc, N\}|}{N^{1/2}} > 0$ ?
 @[category research open, AMS 11]
 theorem erdos_12.parts.i : (∃ (A : Set ℕ), IsGood A ∧
     (0 : ℝ) < Filter.atTop.liminf
-      (fun N => ((Finset.Icc 1 N).filter (· ∈ A)).card / (N : ℝ).sqrt)) ↔ answer(sorry) := by
+      (fun N => (A.bdd N).card / (N : ℝ).sqrt)) ↔ answer(sorry) := by
   sorry
 
 /--
@@ -58,8 +62,8 @@ such that there are always infinitely many $N$
 with $|A \cap \{1, \dotsc, N\}| < N^{1−c}$?
 -/
 @[category research open, AMS 11]
-theorem erdos_12.parts.ii : (∀ (A : Set ℕ), IsGood A → ∃ c > (0 : ℝ),
-  {N : ℕ|  ((Finset.Icc 1 N).filter (· ∈ A)).card < (N : ℝ) ^ (1 - c)}.Infinite) ↔ answer(sorry) := by
+theorem erdos_12.parts.ii : (∃ c > (0 : ℝ), ∀ (A : Set ℕ), IsGood A →
+  {N : ℕ| (A.bdd N).card < (N : ℝ) ^ (1 - c)}.Infinite) ↔ answer(sorry) := by
   sorry
 
 /--
@@ -71,7 +75,11 @@ theorem erdos_12.parts.iii :
     (∀ (A : Set ℕ), IsGood A → Summable (fun (n : A) ↦ (1 / n : ℝ))) ↔ answer(sorry) := by
   sorry
 
-/-- Erdős and Sárközy proved that such an $A$ must have density 0. -/
+/--
+Erdős and Sárközy proved that such an $A$ must have density 0.
+[ErSa70] Erd\H os, P. and Sárk\"ozi, A., On the divisibility properties of sequences of integers.
+    Proc. London Math. Soc. (3) (1970), 97-101
+-/
 @[category research solved, AMS 11]
 theorem erdos_12.variants.density_0 (A : Set ℕ) (hA : IsGood A) : A.HasDensity 0 := by
   sorry
