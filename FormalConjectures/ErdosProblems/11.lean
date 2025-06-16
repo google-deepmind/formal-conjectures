c5705526927d34edb9dff01ae5379a7bc91f263d
liuman <zhenninghimme@gmail.com>
2025-06-16 18:11:03 +0100

    Fix typo with powers of 2

--- FormalConjectures/ErdosProblems/11.lean
+++ FormalConjectures/ErdosProblems/11.lean
@@ -28,7 +28,7 @@ Is every odd n the sum of a squarefree number and a power of 2?
 
 @[category research open, AMS 11]
 theorem erdos_11 (n : ℕ) (hn : Odd n) :
-    ∃ k l : ℕ , Squarefree k ∧ n = k + 2**l := by
+    ∃ k l : ℕ , Squarefree k ∧ n = k + 2^l := by
   sorry
 
 /--
@@ -38,7 +38,7 @@ is not divisible by 4
 -/
 @[category research open, AMS 11]
 theorem erdos_11_i (n : ℕ) (hn : Odd n) (hn_2 : n % 4 ≠ 0):
-    ∃ k l : ℕ , Squarefree k ∧ n = k + 2**l := by
+    ∃ k l : ℕ , Squarefree k ∧ n = k + 2^l := by
   sorry
 
 /--
@@ -46,5 +46,5 @@ Erdős thought that proving this with two powers of 2 is perhaps easy, and could
 -/
 @[category research open, AMS 11]
 theorem erdos_11_ii (n : ℕ) (hn : Odd n):
-    ∃ k l m: ℕ , Squarefree k ∧ n = k + 2**l + 2**m := by
+    ∃ k l m: ℕ , Squarefree k ∧ n = k + 2^l + 2^m := by
   sorry
