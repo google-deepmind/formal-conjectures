38feba5aafee9471afed67a4d8742a87a25a1473
zhenningdavidliu <zhenninghimme@gmail.com>
2025-06-18 16:58:43 +0100

    Update FormalConjectures/ErdosProblems/11.lean
    Fix remove redundant space
    
    Co-authored-by: Salvatore Mercuri <47568553+smmercuri@users.noreply.github.com>

--- FormalConjectures/ErdosProblems/11.lean
+++ FormalConjectures/ErdosProblems/11.lean
@@ -25,7 +25,6 @@ import Mathlib.Data.Nat.Squarefree
 /--
 Is every odd n the sum of a squarefree number and a power of 2?
 -/
-
 @[category research open, AMS 11]
 theorem erdos_11 (n : ℕ) (h_odd : Odd n) :
     ∃ k l : ℕ, Squarefree k ∧ n = k + 2 ^ l := by
