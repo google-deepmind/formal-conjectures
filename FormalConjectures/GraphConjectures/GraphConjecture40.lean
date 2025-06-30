435e0078bc75e7a31c5aba5789672231d5e4cc6e
Henryk Michalewski <henrykm@google.com>
2025-06-30 21:25:07 +0000

    Fixing typo in Conjecture40

--- FormalConjectures/GraphConjectures/GraphConjecture40.lean
+++ FormalConjectures/GraphConjectures/GraphConjecture40.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2025 Henryk Michalewski. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Henryk Michalewski
+-/
+
 import FormalConjectures.GraphConjectures.Imports
 import FormalConjectures.ForMathlib.Combinatorics.SimpleGraph.GraphConjectures.Definitions
 import FormalConjectures.ForMathlib.Combinatorics.SimpleGraph.GraphConjectures.Invariants
@@ -6,19 +12,19 @@ namespace SimpleGraph
 
 variable {α : Type*} [Fintype α] [DecidableEq α] (G : SimpleGraph α)
 
--- f(G) is provided by `GraphConjectures.Definitions`.
+/- f(G) is provided by `GraphConjectures.Definitions`. -/
 
--- b(G) is provided by `GraphConjectures.Definitions`.
+/- b(G) is provided by `GraphConjectures.Definitions`. -/
 
-/--
-WOWII Conjecture 40 (open):
+/-!
+# WOWII Conjecture 40 (open)
+
 For a nontrivial connected graph `G` the size `f(G)` of a largest induced forest
 satisfies `f(G) ≥ ceil((p(G) + b(G) + 1)/2)` where `p(G)` is the path cover
 number and `b(G)` is the largest induced bipartite subgraph size.
 -/
--- Theorem: If G is a simple connected graph on more than one vertex,
--- then f(G) ≥ ⌈(p(G) + b(G) + 1) / 2⌉
 theorem conjecture40 (h_conn : G.Connected) (h_nontrivial : 1 < Fintype.card α) :
   f G ≥ ⌈((p G + b G + 1) / 2)⌉ := by sorry
 
 end SimpleGraph
+
