2ae34a603b6b8d2293b734db06907b54e526c113
m-ow <maxonomar@ciencias.unam.mx>
2025-06-30 10:09:37 -0600

    modify style

--- FormalConjectures/Wikipedia/EulerSumOfPowers.lean
+++ FormalConjectures/Wikipedia/EulerSumOfPowers.lean
@@ -32,5 +32,5 @@ but remains open for $k \geq 6$.
 -/
 @[category research open, AMS 11]
 theorem eulers_conjecture (n k b : ℤ) (hn : 1 < n) (hk : 1 < k) (a: ℤ → ℝ)
-    (hsum : ∑ i ∈ Finset.Icc 1 n, (a ^ k) i = b ^ k) : n ≥ k := by
+    (hsum : ∑ i ∈ Finset.Icc 1 n, (a^k) i = b^k) : n ≥ k := by
   sorry
