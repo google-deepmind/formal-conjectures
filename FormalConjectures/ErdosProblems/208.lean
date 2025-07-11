351fed39c158d5e26b45027340abd4e1a78e759d
Calle Sonne <csonne@google.com>
2025-07-11 06:39:21 +0000

    add

---
+++ FormalConjectures/ErdosProblems/208.lean
@@ -0,0 +1,32 @@
+import OpenConjectures.Util.ProblemImports
+
+/-!
+# Erdős Problem 208
+
+*Reference:* [erdosproblems.com/208](https://www.erdosproblems.com/208)
+-/
+open Filter Real
+
+-- TODO: Need to fix wording on website...
+
+/--
+Let `s1 < s2 < ⋯` be a sequence of squarefree numbers. Is it true that, for any `ϵ > 0` and large `n`,
+`s (n+1) − s n ≪_ϵ (s n)^ε`?
+-/
+theorem erdos_208.i (s : ℕ → ℕ) (hs₀ : StrictMono s) (hs₁ : ∀ n, Squarefree (s n)) :
+    ∀ ε > 0, ∃ C > 0, ∀ᶠ n in atTop, s (n + 1) - s n < C * (s n)^ε := sorry
+
+/--
+Let `s1 < s2 < ⋯` be a sequence of squarefree numbers. Is it true that
+`s (n + 1) - s n ≤ (1 + o(1)) * (π^2 / 6) * log (s n) / log (log (s n))`?
+
+-/
+theorem erdos_208.ii (s : ℕ → ℕ) (hs₀ : StrictMono s) (hs₁ : ∀ n, Squarefree (s n)) :
+    ∃ (c : ℕ → ℝ), (c =o[atTop] (fun n ↦ (1 : ℝ))) ∧
+    ∀ᶠ n in atTop, s (n + 1) - s n ≤ (1 + (c n)) * (π^2 / 6) * log (s n) / log (log (s n))  := sorry
+
+/--
+Erdős says perhaps `s (n+1) − s n ≪ log (s n)`, but he is 'very doubtful'.
+-/
+theorem erdos_208.variants.log_bound (s : ℕ → ℕ) (hs₀ : StrictMono s) (hs₁ : ∀ n, Squarefree (s n)) :
+    (fun n ↦ (s (n + 1) - s n : ℝ)) =O[atTop] fun n ↦ log (s n) := sorry
