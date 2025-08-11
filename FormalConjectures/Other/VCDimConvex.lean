41201d544fa3850111b641e7969f11eca65cb644
Yaël Dillies <yael.dillies@gmail.com>
2025-08-11 11:48:55 +0000

    feat: state results and conjectures on VCₘ dimension of convex sets in ℝⁿ VC dimension of a set family is an old notion in learning theory. VC dimension of a *set* in a group (defined as the VC dimension of its family of translates) is a more recent notion motivated by additive combinatorics. VCₙ dimension of a set family is a very recent notion that appeared in the context of model theory. Therefore very few questions have been asked and answered at the intersection of both, i.e. about the VCₙ dimension of a family of translates
    This PR offers some conjectures in this direction. The conjectures are all mine and do not appear in the literature. They are very easily stated due to the elementary nature of VCₙ dimension.

---
+++ FormalConjectures/Other/VCDimConvex.lean
@@ -0,0 +1,64 @@
+/-
+Copyright 2025 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+import Mathlib.Analysis.Convex.Basic
+import Mathlib.Data.Real.Basic
+
+/-!
+# VCₙ dimension of convex sets in ℝⁿ, ℝⁿ⁺¹, ℝⁿ⁺²
+
+In the literature it is known that every convex set in ℝ² has VC dimension at most 3,
+and there exists a convex set in ℝ³ with infinite VC dimension (even more strongly,
+which shatters an infinite set).
+
+This file states that every convex set in ℝⁿ has finite VCₙ dimension, constructs a convex set in
+ℝⁿ⁺² with infinite VCₙ dimension (even more strongly, which n-shatters an infinite set),
+and conjectures that every convex set in ℝⁿ⁺¹ has finite VCₙ dimension.
+-/
+
+/-! ### What's known -/
+
+/-- Every convex set in ℝ² has VC dimension at most 3. -/
+lemma vc_lt_four_of_convex_r2 {C : Set (Fin 2 → ℝ)} (hC : Convex ℝ C)
+    {x : Fin 4 → Fin 2 → ℝ} {y : Set (Fin 4) → Fin 2 → ℝ}
+    (hxy : ∀ i s, x i + y s ∈ C ↔ i ∈ s) : False := sorry
+
+/-- There exists a set of infinite VC dimension in ℝ³. -/
+lemma exists_convex_r3_vc_eq_infty :
+    ∃ (C : Set (Fin 3 → ℝ)) (hC : Convex ℝ C) (x : ℕ → Fin 3 → ℝ) (y : Set ℕ → Fin 3 → ℝ),
+      ∀ i s, x i + y s ∈ C ↔ i ∈ s := sorry
+
+/-! ### What's new -/
+
+/-- There exists a set of infinite VCₙ dimension in ℝⁿ⁺². -/
+lemma exists_convex_rn_add_two_vc_n_eq_infty (n : ℕ) :
+    ∃ (C : Set (Fin (n + 2) → ℝ)) (hC : Convex ℝ C) (x : Fin n → ℕ → Fin (n + 2) → ℝ)
+      (y : Set (Fin n → ℕ) → Fin (n + 2) → ℝ),
+        ∀ i s, ∑ k, x k (i k) + y s ∈ C ↔ i ∈ s := sorry
+
+/-! ### Conjectures -/
+
+/-- Every convex set in ℝ³ has VC₂ dimension at most 1.
+
+In fact, this set n-shatters an infinite set. -/
+lemma vc2_lt_two_of_convex_r3 {C : Set (Fin 3 → ℝ)} (hC : Convex ℝ C)
+    {x y : Fin 2 → Fin 3 → ℝ} {z : Set (Fin 2 × Fin 2) → Fin 3 → ℝ}
+    (hxy : ∀ i j s, x i + y j + z s ∈ C ↔ (i, j) ∈ s) : False := sorry
+
+/-- Every convex set in ℝⁿ⁺¹ has VC₂ dimension at most 1. -/
+lemma exists_vcn_le_of_convex_rn_add_one (n : ℕ) :
+    ∃ d : ℕ, ∀ (C : Set (Fin (n + 1) → ℝ)) (hC : Convex ℝ C) (x : Fin n → ℕ → Fin (n + 1) → ℝ)
+      (y : Set (Fin n → ℕ) → Fin (n + 1) → ℝ) (hxy : ∀ i s, ∑ k, x k (i k) + y s ∈ C ↔ i ∈ s),
+        False := sorry
