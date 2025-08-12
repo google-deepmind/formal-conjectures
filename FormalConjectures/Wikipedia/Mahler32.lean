fb6638e2de932d0bdfd214cb2847b0cfdc8c42a5
Paul Lezeau <lezeau@google.com>
2025-08-12 10:58:04 +0000

    Add docstring and fixes

--- FormalConjectures/Wikipedia/Mahler32.lean
+++ FormalConjectures/Wikipedia/Mahler32.lean
@@ -22,22 +22,34 @@ import FormalConjectures.Util.ProblemImports
 *Reference:* [Wikipedia](https://en.wikipedia.org/wiki/Mahler%27s_3/2_problem)
 -/
 
+/-- For a real number `α`, define `Ω(α)` as
+$$
+\Omega (\alpha )=\inf _{\theta > 0}\left({\limsup _{n\rightarrow \infty }\left\lbrace
+{\theta \alpha ^{n}}\right\rbrace -\liminf _{n\rightarrow \infty }\left\lbrace {\theta \alpha ^{n}}\right\rbrace }\right).
+$$
+-/
 private noncomputable def Ω (α : ℝ) : ℝ :=
   ⨅ (θ : ℝ) (_ : 0 < θ), Filter.atTop.limsup (fun n ↦ Int.fract (θ * α ^ n))
     - Filter.atTop.liminf (fun n ↦ Int.fract (θ * α ^ n))
 
+/-- A Z-number is a real number `x` such that the fractional parts of `x(3/2)^n` are less than
+`1/2` for all positive integers `n`. -/
 private def IsZNumber (x : ℝ) : Prop :=
   ∀ n > 0, Int.fract (x * (3 / 2 : ℝ) ^ n) < 1 / 2
 
+/-- The **Mahler Conjecture** states that there are no Z-numbers. -/
 @[category research open, AMS 11]
 theorem mahler_conjecture (x : ℝ) (hx : IsZNumber x) : False := by
   sorry
 
+/-- If Mahler's conjecture is true, i.e. there are no `Z` numbers, then `Ω(3/2)` exceeds `1/2`.  -/
 @[category undergraduate, AMS 11]
-theorem mahler_conjecture.variants.consequence : 1 / 2 < Ω (3 / 2) := by
+theorem mahler_conjecture.variants.consequence (H : type_of% mahler_conjecture) :
+    1 / 2 < Ω (3 / 2) := by
   sorry
 
+/-- It is known that for all rational `p/q > 1` in lowest terms, we have `Ω(p/q) > 1/p`. -/
 @[category research solved, AMS 11]
-theorem mahler_conjecture.variants.flatto_lagarias_pollington (p q : ℕ) (hp : 1 < p) (hq : 1 < q)
-    (hpq : p.Coprime q) : 1 / p < Ω (p / q) := by
+theorem mahler_conjecture.variants.flatto_lagarias_pollington (p q : ℕ) (hp : 1 ≤ p) (hq : 1 ≤ q)
+    (hpq : p.Coprime q) (hpq' : q ≤ p) : 1 / p < Ω (p / q) := by
   sorry
