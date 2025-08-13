a4d8bd118fef132d73f6e93aa0a4eff4e5260ff2
Paul Lezeau <lezeau@google.com>
2025-08-13 09:52:24 +0000

    Remove redundant lemmas, add comments

--- FormalConjectures/ForMathlib/Computability/TuringMachine/PostTuringMachine.lean
+++ FormalConjectures/ForMathlib/Computability/TuringMachine/PostTuringMachine.lean
@@ -3,23 +3,12 @@ import Mathlib.Logic.Relation
 
 open Turing
 
-lemma Option.bind_iterate {α} (f : α → Option α) (a : Option α) (n : ℕ)  :
-    (Option.bind · f)^[n+1] a = Option.bind ((Option.bind · f)^[n] a) f := by
-  induction n with
-  | zero => simp
-  | succ n ih => rw [Function.iterate_succ', Function.comp_apply, ih]
-
-lemma Option.bind_iterate' {α} (f : α → Option α) (a : Option α) (n : ℕ)  :
-    (Option.bind · f)^[n+1] a = (Option.bind · f)^[n] (a.bind f) := by
-  induction n generalizing a with
-  | zero => simp
-  | succ n ih => rw [Function.iterate_succ, Function.comp_apply, ih]
-
 lemma dom_of_apply_eq_none  {σ : Type*} {f : σ → Option σ} {s : σ} (hf : f s = none) :
     s ∈ Turing.eval f s := by
   apply PFun.fix_stop
   simp [hf]
 
+#check PFun.fix
 @[simp]
 theorem Turing.apply_get_eval {σ : Type*} {f : σ → Option σ} {s : σ} (H : (Turing.eval f s).Dom) :
     f ((Turing.eval f s).get H) = none := by
@@ -35,15 +24,14 @@ theorem Part.get_eq_get {σ : Type*} {a b : Part σ} (ha : a.Dom) (hb : a.get ha
   ext c
   rw [← Part.eq_get_iff_mem ha, ← Part.eq_get_iff_mem hb', hb]
 
-theorem dom_eval_of_dom {σ : Type*} {f : σ → Option σ} {s : σ} (H : (Turing.eval f s).Dom) :
-    (Turing.eval f ((Turing.eval f s).get H)).Dom := by
-  suffices Turing.eval f ((Turing.eval f s).get H) = Turing.eval f s by rwa [this]
-  have : (Turing.eval f s).get H ∈ Turing.eval f ((Turing.eval f s).get H) := by
-    apply dom_of_apply_eq_none
-    simp
+-- TODO(Paul-Lez): also prove this for `PFun.fix`/golf using the `PFun.fix` API
+theorem Turing.eval_get_eval {σ : Type*} {f : σ → Option σ} {s : σ} (H : (Turing.eval f s).Dom) :
+    Turing.eval f ((Turing.eval f s).get H) = Turing.eval f s := by
   symm
-  apply Part.get_eq_get H this
+  apply Part.get_eq_get H (dom_of_apply_eq_none ?_)
+  simp
 
+-- TODO(Paul-Lez): also prove this for `PFun.fix`/golf using the `PFun.fix` API
 theorem Turing.eval_eq_eval {σ : Type*} {f : σ → Option σ} {a a' : σ} (H : f a = some a'):
     Turing.eval f a = Turing.eval f a' := by
   apply reaches_eval
@@ -64,7 +52,7 @@ theorem eval_dom_iff {σ : Type*} {f : σ → Option σ} {s : σ} :
     | succ n ih =>
       obtain ha | ⟨a', ha'⟩ := (f s).eq_none_or_eq_some
       · simpa [Part.dom_iff_mem] using ⟨s, dom_of_apply_eq_none ha⟩
-      · simp_rw [Option.bind_iterate', Option.some_bind] at hn ih
+      · simp_rw [Function.iterate_succ, Function.comp_apply, Option.some_bind] at hn ih
         simp_rw [ha', Option.some_bind] at hn
         have ih := @ih a' hn
         rwa [Turing.eval_eq_eval ha']
@@ -77,4 +65,4 @@ theorem eval_dom_iff {σ : Type*} {f : σ → Option σ} {s : σ} :
     · obtain ⟨n, hn⟩ := h a' ha' (by rwa [←Turing.eval_eq_eval ha'])
       use n + 1
       simp only [Function.iterate_succ, Function.comp_apply, Option.some_bind] at hn
-      simp [Option.bind_iterate', Option.some_bind, ha', Option.bind_iterate', Option.some_bind, hn]
+      simp [ha', hn]
