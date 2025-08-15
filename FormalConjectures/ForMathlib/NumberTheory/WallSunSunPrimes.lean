ff407548550515f94dd1bbed2248d5c395034476
Paul Lezeau <lezeau@google.com>
2025-08-15 16:40:08 +0000

    Refactor definitions

--- FormalConjectures/ForMathlib/NumberTheory/WallSunSunPrimes.lean
+++ FormalConjectures/ForMathlib/NumberTheory/WallSunSunPrimes.lean
@@ -14,10 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -/
 
-import Mathlib.NumberTheory.LegendreSymbol.Basic
+import Mathlib.NumberTheory.LegendreSymbol.JacobiSymbol
 
-/-!
-# Problem 4.65
+/-! #
 
 *References:*
 - [Wikipedia, Lucas_sequence](https://en.wikipedia.org/wiki/Lucas_sequence)
@@ -29,32 +28,37 @@ import Mathlib.NumberTheory.LegendreSymbol.Basic
 $L_0 = 2$, $L_1=1$, $L_{n+2} = L_{n+1}+L_n$
 -/
 def lucasNumber : ℕ → ℤ
-| 0 => 2
-| 1 => 1
-| (n+2) => lucasNumber (n+1) + lucasNumber n
+  | 0 => 2
+  | 1 => 1
+  | (n + 2) => lucasNumber (n + 1) + lucasNumber n
 
 /--
 **The Lucas sequence of the first kind**
 $U_0(a,b) = 0$, $U_1(a,b)=1$, $U_{n+2}(a,b)=aU_{n+1}(a,b)-bU_n(a,b)$
 -/
 def lucasUNumberPQ (a b : ℕ) : ℕ → ℤ
-| 0 => 0
-| 1 => 1
-| (n+2) => a * lucasUNumberPQ a b (n+1) - b * lucasUNumberPQ a b n
+  | 0 => 0
+  | 1 => 1
+  | (n + 2) => a * lucasUNumberPQ a b (n + 1) - b * lucasUNumberPQ a b n
 
 /--
 **Wall–Sun–Sun prime**
-A prime $p$ is a Wall–Sun–Sun prime if and only if $L_p \equiv 1 \pmod{p^2}$, where $L_p$ is Lucas numbers.
-(One of the equivalent definitions)
+A prime $p$ is a Wall–Sun–Sun prime if and only if $L_p \equiv 1 \pmod{p^2}$, where $L_p$ is
+the $p$-th Lucas number.
 -/
-def IsWallSunSunPrime (p : ℕ) : Prop :=
-  p.Prime ∧ (lucasNumber p).ModEq (p^2) 1
+structure IsWallSunSunPrime (p : ℕ) : Prop where
+  prime : p.Prime
+  lucasNumber_modeq : lucasNumber p ≡ 1 [ZMOD (p ^ 2)]
 
 /--
 **Lucas–Wieferich prime**
+Ref: https://en.wikipedia.org/wiki/Lucas%E2%80%93Wieferich_prime
 A Lucas–Wieferich prime associated with $(a,b)$ is a prime $p$ such $U_{p-\varepsilon}(a,b) \equiv 0 \pmod{p^2}$
-where $U(a,b)$ is the Lucas sequence of the first kind and $\varepsilon$ is the Legendre symbol.
+where $U(a,b)$ is the Lucas sequence of the first kind and $\varepsilon$ is the Legendre symbol
+$\left({\tfrac {P^{2}-4Q}{p}}\right)$
 -/
-def IsLucasWieferichPrime (a b p : ℕ) : Prop :=
-  letI index := (p : ℤ) - legendreSym p (a^2 - 4*b) -- legendreSym needs [Fact (p.Prime)] 
-  p.Prime ∧ (lucasUNumberPQ a b index.toNat).ModEq (p^2) 1
+structure IsLucasWieferichPrime (a b p : ℕ) : Prop where
+  prime : p.Prime
+  modeq :
+    letI index := (p : ℤ) - jacobiSym p (a^2 - 4*b)
+    lucasUNumberPQ a b index.toNat ≡ 0 [ZMOD (p^2)]
