ffc40535b21c60e96cc01e81469d260ccdc99027
Yoh Tanimoto <hoyt@jcom.home.ne.jp>
2025-08-18 15:44:28 +0200

    Update InvariantSubspaceProblem.lean

--- FormalConjectures/Wikipedia/InvariantSubspaceProblem.lean
+++ FormalConjectures/Wikipedia/InvariantSubspaceProblem.lean
@@ -22,11 +22,11 @@ import FormalConjectures.Util.ProblemImports
 *Reference:* [Wikipedia](https://en.wikipedia.org/wiki/Invariant_subspace_problem)
 -/
 
-variable {H : Type*} [NormedAddCommGroup H] [InnerProductSpace ℂ H] [CompleteSpace H] [TopologicalSpace.SeparableSpace H]
+variable {H : Type*} [NormedAddCommGroup H]
 
-/-- `ClosedInvariantSubspace T` is the type of closed subspaces of complex vector space `H`
-that are invariant under the action of linear map `x`. -/
-structure ClosedInvariantSubspace (T : H →L[ℂ] H) where
+/-- `ClosedInvariantSubspace T` is the type of non-trivial (different from `H` and `{0}`) closed
+subspaces of complex vector space `H` that are invariant under the action of linear map `T`. -/
+structure ClosedInvariantSubspace [Module ℂ H] (T : H →L[ℂ] H) where
   toSubspace : Submodule ℂ H
   ne_bot : toSubspace ≠ ⊥
   ne_top : toSubspace ≠ ⊤
@@ -38,6 +38,43 @@ Determine whether every bounded linear operator `T : H → H` in a separable Hil
 non-trivial closed `T`-invariant subspace: a closed linear subspace `W` of `H`, which is different
 from `{ 0 }` and from `H`, such that `T ( W ) ⊂ W`.-/
 @[category research open, AMS 47]
-theorem Invariant_subspace_problem (T : H →L[ℂ] H) : Nonempty (ClosedInvariantSubspace T) := by
+theorem Invariant_subspace_problem [InnerProductSpace ℂ H] [TopologicalSpace.SeparableSpace H]
+    [CompleteSpace H] (T : H →L[ℂ] H) : Nonempty (ClosedInvariantSubspace T) := by
   sorry
 
+/--
+Every (bounded) linear operator `T : H → H` in a finite-dimensional linear space `H` has a
+non-trivial (closed) `T`-invariant subspace. This can be solved using the Jordan normal form,
+which is not yet in [mathlib](https://leanprover-community.github.io/undergrad_todo.html). -/
+@[category research solved, AMS 47]
+theorem Invariant_subspace_problem_finite_dimensional [Module ℂ H] (h : FiniteDimensional ℂ H)
+    (T : H →L[ℂ] H) : Nonempty (ClosedInvariantSubspace T) := by
+  sorry
+
+/--
+Every bounded linear operator `T : H → H` in a non-separable Hilbert space `H` has a
+non-trivial closed `T`-invariant subspace. Such an invariant space is given by considering the
+closure of the linear span of the orbit of any single non-zero vector. -/
+@[category research solved, AMS 47]
+theorem Invariant_subspace_problem_non_separable [InnerProductSpace ℂ H] [CompleteSpace H]
+    (h : ¬TopologicalSpace.SeparableSpace H) (T : H →L[ℂ] H) :
+    Nonempty (ClosedInvariantSubspace T) := by
+  sorry
+
+/--
+Every normal linear operator `T : H → H` in a Hilbert space `H` has a non-trivial closed
+`T`-invariant subspace. One can take any spectral subspace of `T`. -/
+@[category research solved, AMS 47]
+theorem Invariant_subspace_problem_normal_operator [InnerProductSpace ℂ H] [CompleteSpace H]
+    (T : H →L[ℂ] H) [IsStarNormal T]:
+    Nonempty (ClosedInvariantSubspace T) := by
+  sorry
+
+/--
+There exists a bounded linear operator `T` on the l1 space `(lp (fun (_ : ℕ) => ℂ) 1))` without
+non-trivial closed `T`-invariant subspace. [Read 1985](https://doi.org/10.1112/blms/17.4.305) -/
+@[category research solved, AMS 47]
+theorem Invariant_subspace_problem_l1 :
+    ∃ (T : (lp (fun (_ : ℕ) => ℂ) 1) →L[ℂ] (lp (fun (_ : ℕ) => ℂ) 1)),
+    IsEmpty (ClosedInvariantSubspace T) := by
+  sorry
