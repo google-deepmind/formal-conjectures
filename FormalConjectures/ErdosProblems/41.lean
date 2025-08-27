2b6f3b81a348ba596b36c874babeb8193c2fadc9
zhenningdavidliu <zhenninghimme@gmail.com>
2025-08-27 16:20:17 +0100

    Fix typos FormalConjectures/ErdosProblems/41.lean
    Co-authored-by: Salvatore Mercuri <47568553+smmercuri@users.noreply.github.com>

--- FormalConjectures/ErdosProblems/41.lean
+++ FormalConjectures/ErdosProblems/41.lean
@@ -38,7 +38,7 @@ private noncomputable def Set.bdd (A : Set ℕ) (N : ℕ) : Finset ℕ :=
     Finset.Icc 1 N |>.filter (· ∈ A)
 
 /--
-Let `A` ⊆ ℕ be an infinite set such that the triple sums `a + b + c` are all distinct for `a, b, c` in `A` (aside from the trivial coincidences). Is it true that `lim inf n → ∞ |A ∩ {1, …, N}| / N^(1/3) = 0`?
+Let `A ⊆ ℕ` be an infinite set such that the triple sums `a + b + c` are all distinct for `a, b, c` in `A` (aside from the trivial coincidences). Is it true that `lim inf n → ∞ |A ∩ {1, …, N}| / N^(1/3) = 0`?
 -/
 @[category research open, AMS 11]
 theorem erdos_41 (A : Set ℕ) (h_triple : NtupleCondition A 3) (h_infinite : A.Infinite) : 
