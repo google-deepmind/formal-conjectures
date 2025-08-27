dcdb9c861eff6c9e6c87c4afca2fc997b9ebabb4
Paul Lezeau <lezeau@google.com>
2025-08-27 09:10:30 +0000

    remove duplicate

--- FormalConjectures/Wikipedia/Transcendental.lean
+++ FormalConjectures/Wikipedia/Transcendental.lean
@@ -195,10 +195,3 @@ $\Gamma(1/n)$ for `n ≥ 2` is transcendental.
 @[category research open, AMS 33]
 theorem transcendental_gamma_one_div (n : ℕ) (hn : 2 ≤ n) : Transcendental ℚ (1 / n : ℝ).Gamma := by
   sorry
-
-/--
-Catalan's constant $$G = \sum_{n=0}^∞ (-1)^n / (2n + 1)^2$$ is transcendental.
--/
-@[category research open, AMS 33]
-theorem transcendental_catalanConstant : Transcendental ℚ catalanConstant := by
-  sorry
