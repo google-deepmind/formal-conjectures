e1351fd122fb55e53a22d96e12113adb03df98d6
Divyanshu Ranjan <idivyanshu.ranjan@gmail.com>
2025-09-21 13:51:07 +0000

    Add formalization of Erdős Problem 188

---
+++ FormalConjectures/ErdosProblems/188.lean
@@ -0,0 +1,43 @@
+/-
+Copyright 2025 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+import FormalConjectures.Util.ProblemImports
+
+/-!
+# Erdős Problem 188
+
+*Reference:* [erdosproblems.com/188](https://www.erdosproblems.com/188)
+-/
+
+namespace Erdos188
+
+/--
+The set of numbers ($k$) such that $\mathbb{R}^2$ can be red/blue coloured with no pair of red
+points unit distance apart, and no $k$-term arithmetic progression of blue points with distance 1.
+-/
+def erdos_188.s := { k : ℕ | ∃ blue : Set ℂ,
+    (Set.univ \ blue).Pairwise (fun c₁ c₂ => dist c₁ c₂ ≠ 1) ∧
+    ¬ (∃ bs ⊆ blue, (∃ s, bs.IsAPOfLengthWith k s 1)) }
+
+/--
+What is the smallest $k$ such that $\mathbb{R}^2$ can be red/blue coloured with no pair of red
+points unit distance apart, and no $k$-term arithmetic progression of blue points with distance 1?
+-/
+@[category research open, AMS 5]
+theorem erdos_188 : IsEmpty erdos_188.s ∨ IsLeast erdos_188.s answer(sorry)
+    := by sorry
+
+end Erdos188
