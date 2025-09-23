5899febc4a75e98c2f113fd10b00c983c9f23a52
Divyanshu Ranjan <idivyanshu.ranjan@gmail.com>
2025-09-23 15:59:27 +0000

    Few more (solved?) conjectures

--- FormalConjectures/ErdosProblems/188.lean
+++ FormalConjectures/ErdosProblems/188.lean
@@ -33,11 +33,32 @@ def s := { k : ℕ | ∃ blue : Set ℂ,
     ¬ (∃ bs ⊆ blue, (∃ s, bs.IsAPOfLengthWith k s 1)) }
 
 /--
+Old and new problems and results in combinatorial number theory by Erdős & Graham (Page 14, 15):
+
+It has been shown that there is a large $M$ so that it is possible to partition $\mathbb{E}^2$ into
+two sets $A$ and $B$ so that $A$ contains no pair of points with distance 1 and $B$ contains no A.P.
+of length $M$.
+-/
+@[category research solved, AMS 5]
+theorem erdos_188.nonempty : s.Nonempty := by
+  sorry
+
+/--
+Old and new problems and results in combinatorial number theory by Erdős & Graham (Page 15):
+
+How small can $M$ be made? The only estimate currently known is that $M$ ≤ 10000000 (more or less).
+In the other direction, it has just been shown by R. Juhász [Ju (79)] that we must have $M$ ≥ 5.
+-/
+@[category research solved, AMS 5]
+theorem erdos_188.estimate : (∀ k, k ∈ s → 5 ≤ k) ∧ (∃ k ∈ s, k ≤ 10000000) := by
+  sorry
+
+/--
 What is the smallest $k$ such that $\mathbb{R}^2$ can be red/blue coloured with no pair of red
 points unit distance apart, and no $k$-term arithmetic progression of blue points with distance 1?
 -/
 @[category research open, AMS 5]
-theorem erdos_188 : IsEmpty s ∨ IsLeast s answer(sorry) := by
+theorem erdos_188 : IsLeast s answer(sorry) := by
   sorry
 
 end Erdos188
