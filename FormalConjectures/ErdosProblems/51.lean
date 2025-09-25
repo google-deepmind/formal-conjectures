e14584046ec94f72a792fe03b24596bbe99a6520
Anthony Wang <anthonyywang05@gmail.com>
2025-09-25 06:51:26 +0000

    Convert the whole comment to latex

--- FormalConjectures/ErdosProblems/51.lean
+++ FormalConjectures/ErdosProblems/51.lean
@@ -28,9 +28,9 @@ open scoped Nat
 namespace Erdos51
 
 /--
-Is there an infinite set $A ⊆ ℕ$ such that for every $a ∈ A$,
+Is there an infinite set $A \subset \mathbb{N}$ such that for every $a \in A$,
 there is an integer n such that $\phi(n)=a$, and
-yet if $n_a$ is the smallest such integer, then $\frac{n_a}{a} → ∞$ as $a → ∞$?
+yet if $n_a$ is the smallest such integer, then $\frac{n_a}{a} → \infty$ as $a → \infty$?
 -/
 @[category research open, AMS 11]
 theorem erdos_51 :
