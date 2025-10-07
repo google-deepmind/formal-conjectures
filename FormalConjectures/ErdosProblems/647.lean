456d22e0f042a6f84693677c35302433e790abdc
smmercuri <mercuris@google.com>
2025-10-07 14:50:08 +0000

    Add Erdos 647

---
+++ FormalConjectures/ErdosProblems/647.lean
@@ -0,0 +1,59 @@
+/-
+Copyright 2025 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+import FormalConjectures.Util.ProblemImports
+
+/-!
+# Erdős Problem 647
+
+*Reference:* [erdosproblems.com/145](https://www.erdosproblems.com/647)
+-/
+
+open Filter ArithmeticFunction
+
+/-- Let $\tau(n)$ count the number of divisors of $n$. Is there some $n > 24$ such that
+$$
+  \max_{m < n}(m + \tau(n)) \leq n + 2?
+$$ -/
+@[category research open, AMS 11]
+theorem erdos_647 : (∃ n > 24, iSup fun m : Fin n ↦ m + σ 0 m ≤ n + 2) ↔ answer(sorry) := by
+  sorry
+
+/-- This is true for $n = 24$. -/
+@[category research solved, AMS 11]
+theorem erdos_647.variants.twenty_four : iSup fun m : Fin 24 ↦ m + σ 0 m ≤ 26 := by
+  sorry
+
+/-- Erdős says 'it is extremely doubtful' that there are infinitely many such $n$, and in
+fact suggests that
+$$
+  lim_{n\to\infty} \max_{m < n}(\tau(m) + m − n) = \infty.
+$$ -/
+@[category research open, AMS 11]
+theorem erdos_647.variants.lim :
+    atTop.Tendsto (fun n ↦ iSup fun (m : Fin n) ↦ σ 0 m + m - n) atTop ↔ answer(sorry) := by
+  sorry
+
+/-- Erdős says it 'seems certain' that for every $k$ there are infinitely many $n$
+for which
+$$
+  \max_{n−k < m < n}(m + \tau(m)) ≤ n + 2.
+$$ -/
+@[category research open, AMS 11]
+theorem erdos_647.variants.infinite :
+    (∀ k, { n | iSup fun m : Set.Ioo (n - k) n ↦ ↑m + σ 0 m ≤ n + 2 }.Infinite) ↔
+      answer(sorry) := by
+  sorry
