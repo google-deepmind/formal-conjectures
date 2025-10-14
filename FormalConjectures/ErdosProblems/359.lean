6534453a596718951bf1c43f3a53fcbf62ffddc8
Paul Lezeau <lezeau@google.com>
2025-10-14 11:55:38 +0000

    Fix definition of IsGoodFor

--- FormalConjectures/ErdosProblems/359.lean
+++ FormalConjectures/ErdosProblems/359.lean
@@ -30,14 +30,14 @@ open Filter Asymptotics
 cannot be written as a sum of consecutive terms of `A 0, ..., A j` -/
 def IsGoodFor (A : ℕ → ℕ) (n : ℕ) : Prop := A 0 = n ∧
   ∀ j, IsLeast
-    {m : ℕ | ∀ a b, Finset.Icc a b ⊆ Finset.Iic j → m ≠ ∑ i ∈ Finset.Icc a b, A i}
+    {m : ℕ | A j < m ∧ ∀ a b, Finset.Icc a b ⊆ Finset.Iic j → m ≠ ∑ i ∈ Finset.Icc a b, A i}
     (A <| j + 1)
 
 /-- Let $a_1< a_2 < ⋯ $ be an infinite sequence of integers such that $a_1=1$ and $a_{i+1}$ is the
 least integer which is not a sum of consecutive earlier $a_j$s. Show that $a_k / k \to \infty$. -/
 @[category research open, AMS 11]
 theorem erdos_359.parts.i (A : ℕ → ℕ) (hA : IsGoodFor A 1) :
-    atTop.Tendsto (fun k ↦ A k / k) atTop := by
+    atTop.Tendsto (fun k ↦ (A k : ℝ) / k) atTop := by
   sorry
 
 /-- Let $a_1< a_2 < ⋯ $ be an infinite sequence of integers such that $a_1=1$ and $a_{i+1}$ is the
