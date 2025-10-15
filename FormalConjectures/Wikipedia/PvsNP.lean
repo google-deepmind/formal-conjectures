969aba076e37f8de8614d44067361b798cf4b38d
Bolton Bailey <bolton.bailey@gmail.com>
2025-10-14 20:03:17 -0700

    add more definitions

--- FormalConjectures/Wikipedia/PvsNP.lean
+++ FormalConjectures/Wikipedia/PvsNP.lean
@@ -20,31 +20,83 @@ import FormalConjectures.Util.ProblemImports
 # P vs NP
 
 *Reference:* [Wikipedia](https://en.wikipedia.org/wiki/P_versus_NP_problem)
+Computational Complexity: A Modern Approach by Arora and Barak, 2009.
+TODO (add additional ref information)
 -/
+
+open Computability Turing
 
 namespace PvsNP
 
 /--
-The type of complexity classes over an alphabet `α` is the set of languages over `α`.
+A simple definition to abstract the notion of a poly-time Turing machine into a predicate.
 -/
-def ComplexityClass (α : Type*) := Set (Language α)
+def isComputableInPolyTime {α β : Type} (ea : FinEncoding α) (eb : FinEncoding β)
+  (f : α → β) := Nonempty (TM2ComputableInPolyTime ea eb f)
 
+/--
+The type of complexity classes over an alphabet `α` consists of sets of languages over `α`.
+-/
+abbrev ComplexityClass (α : Type) := Set (Language α)
 
+/--
+Given a finEncoding of a type `α`, we can get a finEncoding of `List α`
+-/
+def listFinEncoding {α : Type} (ea : FinEncoding α) : FinEncoding (List α) := sorry
 
+/--
+This function courtesy of Daniel Weber,
+https://leanprover.zulipchat.com/#narrow/channel/116395-maths/topic/Formalise.20the.20proposition.20P.20.E2.89.A0NP/with/453031558
+-/
+def pairFinEncoding {α β : Type*} (ea : FinEncoding α) (eb : FinEncoding β) :
+    Computability.FinEncoding (α × β) where
+  /- The encoding language is the direct sum of the respective encoding languages -/
+  Γ := ea.Γ ⊕ eb.Γ
+  /- The encoding function is to concatenate the encodings -/
+  encode x := (ea.encode x.1).map .inl ++ (eb.encode x.2).map .inr
+  /- The decoding function is to filter symbols by the encoding languages they correspond to
+  and decode the parts separately. -/
+  decode x := Option.map₂ Prod.mk (ea.decode (x.filterMap Sum.getLeft?))
+                                  (eb.decode (x.filterMap Sum.getRight?))
+  decode_encode x := by
+    simp [List.filterMap_append, ea.decode_encode, eb.decode_encode]
+    sorry
+  ΓFin := inferInstance
 
 /--
 The class P is the set of languages over an alphabet `α`
 decidable in polynomial time by a deterministic Turing machine
 -/
-def P {α : Type*} [Nontrivial α] : ComplexityClass α :=
-  { L | ∃ M : Turing.TM2ComputableInPolyTime
-
+def P {α : Type} [Nontrivial α] [Finite α] (ea : FinEncoding α) : ComplexityClass α :=
+  { L | isComputableInPolyTime
+          (listFinEncoding ea)
+          (Computability.finEncodingBoolBool)
+          (fun x => L x = true) }
 
 /--
-
+The class NP is the set of languages over an alphabet `α`
+such that there exists a polynomial over ℕ and a poly-time Turing machine
+where for all `x`, `L x = true` iff there exists a `w` of length at most `p (length x)`
+such that the Turing machine accepts the pair `(x,w)`.
 -/
+def NP {α : Type} [Nontrivial α] [Finite α] (ea : FinEncoding α) : ComplexityClass α :=
+  { L | ∃ (p : ℕ → ℕ), ∃ R : (List α × List Bool) → Bool,
+      isComputableInPolyTime
+        (pairFinEncoding (listFinEncoding ea) ((listFinEncoding Computability.finEncodingBoolBool)))
+        (Computability.finEncodingBoolBool)
+        (R) ∧
+      ∀ x, L x = true ↔ ∃ w : List Bool, w.length ≤ p (x.length) ∧ R (x, w) = true
+
+   }
+
 @[category research open, AMS 68]
-theorem PvsNP : sorry := by
+theorem P_ne_NP (α : Type) [Nontrivial α] [Finite α] (ea : FinEncoding α) :
+    P ea ≠ NP ea := by
+  sorry
+
+@[category undergraduate, AMS 68]
+theorem P_subset_NP (α : Type) [Nontrivial α] [Finite α] (ea : FinEncoding α) :
+    P ea ⊆ NP ea := by
   sorry
 
 end PvsNP
