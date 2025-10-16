e0e07ced41a1eb83614f119d0c4565185f41b370
szabadka <szabadka@google.com>
2025-10-16 14:43:21 +0200

    Update FormalConjectures/ForMathlib/Combinatorics/SimpleGraph/Balanced.lean
    Co-authored-by: Moritz Firsching <firsching@google.com>

--- FormalConjectures/ForMathlib/Combinatorics/SimpleGraph/Balanced.lean
+++ FormalConjectures/ForMathlib/Combinatorics/SimpleGraph/Balanced.lean
@@ -33,7 +33,7 @@ variable {V : Type*} [Fintype V]
 We call a graph $D$-balanced (or $D$-almost-regular) if the maximum degree is at most $D$ times the
 minimum degree.
 -/
-def IsBalanced (G : SimpleGraph V) (D : ℝ) [DecidableRel G.Adj]: Prop :=
+def IsBalanced (G : SimpleGraph V) (D : ℝ) [DecidableRel G.Adj] : Prop :=
     G.maxDegree <= D * G.minDegree
 
 end SimpleGraph
