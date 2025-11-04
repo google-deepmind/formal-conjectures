77f386ed54b3c39e8e4e2f528f9539c0a6a8acf9
Moritz Firsching <firsching@google.com>
2025-11-04 12:34:12 +0100

    Update FormalConjectures/ErdosProblems/67.lean
    Co-authored-by: Paul Lezeau <paul.lezeau@gmail.com>

--- FormalConjectures/ErdosProblems/67.lean
+++ FormalConjectures/ErdosProblems/67.lean
@@ -47,7 +47,7 @@ exist $d, m \ge 1$ such that $$\left\lvert \sum_{1\leq k\leq m}f(kd)\right\rvert
 This is true, and was proved by Tao [Ta16]
 -/
 @[category research solved, AMS 11]
-theorem erdos_67.variants.complex (f : ℕ → ({c | ‖c‖ = 1} : Set ℂ)) (C : ℝ) (hC : 0 < C) :
+theorem erdos_67.variants.complex (f : ℕ → Metric.sphere (0 : ℂ) 1) (C : ℝ) (hC : 0 < C) :
     ∃ᵉ (d ≥ 1) (m ≥ 1), C < ‖(∑ k ∈ Finset.Icc 1 m, (f (k * d)).1)‖  := by
   sorry
 
