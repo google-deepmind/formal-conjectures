ca5487e17f1c3eac70b0388d59c2aa4c857ab3cb
florianflorianofficial <florianflorianofficial@web.de>
2025-11-20 22:39:19 +0100

    added `↔ answer(sorry)`
    Co-authored-by: Paul Lezeau <paul.lezeau@gmail.com>

--- FormalConjectures/ErdosProblems/680.lean
+++ FormalConjectures/ErdosProblems/680.lean
@@ -35,7 +35,7 @@ p(n+k)>k^2+1,
 where $p(m)$ denotes the least prime factor of $m$?
 -/
 @[category research open, AMS 11]
-theorem erdos_680 : ∃ N, ∀ n ≥ N, ∃ k, Nat.minFac (n + k) > k^2 + 1 := sorry
+theorem erdos_680 : (∃ N, ∀ n ≥ N, ∃ k, Nat.minFac (n + k) > k^2 + 1) ↔ answer(sorry) := sorry
 
 /--
 Can one prove this is false if we replace $k^2+1$ by $e^{(1+\epsilon)\sqrt{k}}+C_\epsilon$, for all
