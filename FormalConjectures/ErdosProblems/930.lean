5caafe9c58247778937321bbbe372b1290da4cb4
Malhar A. Patel <malharalpeshpatel@gmail.com>
2025-11-20 23:33:21 +0000

    Formalize Erdos Problem 930

--- FormalConjectures/ErdosProblems/930.lean
+++ FormalConjectures/ErdosProblems/930.lean
@@ -26,6 +26,28 @@ open Finset Nat
 
 namespace Erdos930
 
+/--
+$n$ is a perfect power if there exist natural numbers $m$ and $l$
+such that $1 < m$, $1 < l$, and $m^l = n$.
+-/
+def PerfectPower (n : ℕ) : Prop :=
+  ∃ m l, 1 < m ∧ 1 < l ∧ m^l = n
 
+/--
+Is it true that, for every $r$, there is a $k$ such that
+if $I_1,\ldots,I_r$ are disjoint intervals of consecutive integers,
+all of length at least $k$, then
+$$
+  \prod_{1\leq i\leq r}\prod_{m\in I_i}m
+$$
+is not a perfect power?
+-/
+@[category research open, AMS 11]
+theorem erdos_930 :
+  ∀ r, ∃ k, ∀ (I : Fin r → Finset ℕ),
+    (∀ i j : Fin r, i ≠ j → Disjoint (I i) (I j)) →
+      (∀ i : Fin r, k ≤ (I i).card ∧ ∃ a, I i = Ico a (a + (I i).card)) →
+        ¬ PerfectPower (∏ i : Fin r, ∏ m ∈ I i, m) := by
+  sorry
 
 end Erdos930
