a05e76cbd9f20a8822f733cdd52618f22b2b93c5
Malhar A. Patel <malharalpeshpatel@gmail.com>
2025-11-21 15:20:52 +0000

    Improve formatting and add reference

--- FormalConjectures/ErdosProblems/952.lean
+++ FormalConjectures/ErdosProblems/952.lean
@@ -19,7 +19,9 @@ import FormalConjectures.Util.ProblemImports
 /-!
 # Erdős Problem 952
 
-*Reference:* [erdosproblems.com/952](https://www.erdosproblems.com/952)
+*References:*
+- [erdosproblems.com/952](https://www.erdosproblems.com/952)
+- [Wikipedia](https://wikipedia.org/wiki/Gaussian_moat)
 -/
 
 
@@ -31,7 +33,7 @@ such that $\lvert x_{n+1}-x_n\rvert \ll 1$?
 -/
 @[category research open, AMS 11]
 theorem erdos_952 :
-  ∃ (x : ℕ → GaussianInt), ∃ (C : ℤ),
+  ∃ (x : ℕ → GaussianInt) (C : ℤ),
     Function.Injective x ∧
       ∀ n, Prime (x n) ∧ (x (n + 1) - x n).norm < C := by
   sorry
