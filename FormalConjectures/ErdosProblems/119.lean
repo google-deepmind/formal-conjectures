9eb8c8a77ae725de6677794d4b88438f7bc77b97
Zehao Yang <williamyangogr@gmail.com>
2025-11-24 21:25:27 +0100

    changes

--- FormalConjectures/ErdosProblems/119.lean
+++ FormalConjectures/ErdosProblems/119.lean
@@ -26,41 +26,47 @@ open Filter Finset Set
 
 namespace Erdos119
 
-/-- Let $z_i$ be an infinite sequence of complex numbers such that $|z_i| = 1$ for all $i \geq 1$. -/
-variable (z : PNat → ℂ) (hz : ∀ i : PNat, ‖z i‖ = 1)
+/-
+Here we use 0-indexing for generality and convenience, while in the original problem
+formulation 1-indexing was used. This change does not affect the meaning of the problem.
+In the description of the problem below we remain faithful to the original one.
+-/
 
-/-- For $n \geq 1$ let $p_n(z) = \prod_{i \leq n} (z - z_i)$. -/
-noncomputable def p (n : PNat) : ℂ → ℂ :=
-    fun w => ∏ i ∈ range n, (w - z ⟨i + 1, by linarith⟩)
+/-- Let $z_i$ be an infinite sequence of complex numbers such that $|z_i| = 1$ for all $i \geq 1$.
+For $n \geq 1$ let $p_n(z) = \prod_{i \leq n} (z - z_i)$. -/
+noncomputable def p (z : ℕ → ℂ) (n : ℕ) : ℂ → ℂ :=
+    fun w => ∏ i ∈ range n, (w - z i)
 
 /-- Let $M_n = \max_{|z| = 1} |p_n(z)|$. -/
-noncomputable def M (n : PNat) : ℝ :=
+noncomputable def M (z : ℕ → ℂ) (n : ℕ) : ℝ :=
     sSup { (‖p z n w‖) | (w : ℂ) (_ : ‖w‖ = 1) }
 
 /-- Question 1:
 
 Is it true that $\limsup M_n = \infty$?
 
-Wagner [Wa80] proved that there is some c > 0 with Mₙ > (log n)ᶜ infintely often.
+Wagner [Wa80] proved that there is some $c > 0$ with $M_n > (\log n)^c$ infintely often.
 
 [Wa80] Wagner, Gerold, On a problem of {E}rdős in {D}iophantine approximation. Bull. London Math. Soc. (1980), 81--88.
 -/
 @[category research solved, AMS 30]
 theorem erdos_119_1 :
-    atTop.limsup (fun n => (M z n : EReal)) = ⊤ ↔ answer(True) := by
+    (∀ (z : ℕ → ℂ) (hz : ∀ i : ℕ, ‖z i‖ = 1),
+      atTop.limsup (fun n => (M z n : EReal)) = ⊤) ↔ answer(True) := by
   sorry
 
 /-- Question 2:
 
 Is it true that there exists $c > 0$ such that for infinitely many $n$ we have $M_n > n^c$?
 
-Beck [Be91] proved that there exists some c > 0 such that max_{n ≤ N} Mₙ > Nᶜ.
+Beck [Be91] proved that there exists some $c > 0$ such that $\max_{n \leq N} M_n > N^c$.
 
 [Be91] Beck, J., The modulus of polynomials with zeros on the unit circle: A problem of Erdős. Annals of Math. (1991), 609-651.
 -/
 @[category research solved, AMS 30]
 theorem erdos_119_2 :
-    ∃ (c : ℝ) (hc : c > 0), Set.Infinite {n : PNat | M z n > n ^ c} ↔ answer(True) := by
+    (∀ (z : ℕ → ℂ) (hz : ∀ i : ℕ, ‖z i‖ = 1),
+      ∃ (c : ℝ) (hc : c > 0), Infinite {n : ℕ | M z n > n ^ c}) ↔ answer(True) := by
   sorry
 
 /-- Question 3:
@@ -69,8 +75,9 @@ Is it true that there exists $c > 0$ such that, for all large $n$, $\sum_{k \leq
 -/
 @[category research open, AMS 30]
 theorem erdos_119_3 :
-    ∃ (c : ℝ) (hc : c > 0), ∀ᶠ n in atTop,
-      ∑ k ∈ range n, M z ⟨k + 1, by linarith⟩ > n ^ (1 + c) ↔ answer(sorry) := by
+    (∀ (z : ℕ → ℂ) (hz : ∀ i : ℕ, ‖z i‖ = 1),
+      ∃ (c : ℝ) (hc : c > 0), ∀ᶠ n in atTop,
+        ∑ k ∈ range n, M z k > n ^ (1 + c)) ↔ answer(sorry) := by
   sorry
 
 end Erdos119
