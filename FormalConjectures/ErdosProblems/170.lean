62076ecf384afbedc99a08a6a69ece93875725a4
eladyn <eladyn@protonmail.com>
2025-11-25 22:21:43 +0100

    add additional parts from erdos170

--- FormalConjectures/ErdosProblems/170.lean
+++ FormalConjectures/ErdosProblems/170.lean
@@ -26,21 +26,23 @@ open scoped Topology
 
 namespace Erdos170
 
-/-- An `N`-perfect ruler is a subset `A ⊆ ℕ` (the marks), such that each positive integer
-`k ≤ N` can be measured, that is, expressed as a difference `k = a₁ - a₀` with `a₁, a₂ ∈ A`. -/
+/-- An $N$-perfect ruler is a finite subset $A \subseteq \mathbb{N}$ (the marks), such that each
+positive integer $k \leq N$ can be measured, that is, expressed as a difference $k = a_1 - a_0$
+with $a_0, a_1 \in A$. The set $A$ is then also called a difference basis w.r.t. $N$. -/
 @[reducible]
 def PerfectRuler (N : ℕ) (A : Finset ℕ) : Prop :=
   ∀ k ∈ Finset.range (N + 1), ∃ᵉ (a₀ ∈ A) (a₁ ∈ A), k = a₁ - a₀
 
-/-- We define the set of all `N`-perfect rulers `A` of length `N`, i.e.
-subsets `A ⊆ {0, …, N}`, s.t. `A` is `N`-perfect. -/
+/-- We define the set of all $N$-perfect rulers $A$ of length $N$, i.e.
+subsets $A \subseteq \{0, \dots, N\}$, s.t. $A$ is $N$-perfect.
+This is also called a restricted difference basis w.r.t. $N$. -/
 def PerfectRulersLengthN (N : ℕ) :
     Finset (Finset ℕ) := (Finset.range (N + 1)).powerset.filter (PerfectRuler N)
 
-/-- The trivial ruler with all marks `{0, …, N}`. -/
+/-- The trivial ruler with all marks $\{0, \dots, N\}$. -/
 abbrev TrivialRuler (N : ℕ) : Finset ℕ := Finset.range (N+1)
 
-/-- Sanity Check: the trivial ruler is actually a perfect ruler if (K ≥ N) -/
+/-- Sanity Check: the trivial ruler is actually a perfect ruler if $K \geq N$ -/
 @[category API, AMS 05]
 lemma trivial_ruler_is_perfect (N : ℕ) : TrivialRuler N ∈ PerfectRulersLengthN N := by
     simp only [PerfectRulersLengthN, Finset.mem_filter, Finset.mem_powerset, Finset.range_subset,
@@ -52,6 +54,21 @@ def F (N : ℕ) : ℕ :=
     Finset.min' (Finset.image Finset.card (PerfectRulersLengthN N))
                 (Finset.image_nonempty.mpr ⟨TrivialRuler N, trivial_ruler_is_perfect N⟩)
 
-/-- The problem is to determine the limit of the sequence `(F N) / √N` as `N → ∞`. -/
+/-- The problem is to determine the limit of the sequence $\frac{F(N)}{\sqrt{N}}$ as $N \to \infty$. -/
 @[category research open, AMS 05]
-lemma erdos170 : Filter.Tendsto (fun N => F N / √N) Filter.atTop (𝓝 answer(sorry)) := sorry
+lemma erdos170 : Filter.Tendsto (fun N => F N / √N) Filter.atTop (𝓝 answer(sorry)) := by sorry
+
+/-- A known lower bound to the limit by Leech [Le56], which is $1.56\dots$. -/
+noncomputable abbrev lower_bound := √(sSup {2 * (1 - Real.sin θ / θ) | θ ≠ 0})
+/-- A known upper bound obtained by constructing Wichmann's Rulers [Wi63]. -/
+noncomputable abbrev upper_bound := √3
+
+/-- The existence of the limit has been proved by Erdős and Gál [ErGa48].
+The lower bound has been proven by Leech [Le56], who refined an argument of Rédei and Rényi.
+The upper bound is due to Wichmann [Wi63]. -/
+@[category research solved, AMS 05]
+lemma erdos170.existing_bounds :
+  ∃ x ∈ Set.Icc lower_bound upper_bound,
+    Filter.Tendsto (fun N => F N / √N) Filter.atTop (𝓝 x) := by sorry
+
+end Erdos170
