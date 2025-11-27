0f211815ddc08cda3680d95547ca1568123fea96
Calle Sonne <csonne@google.com>
2025-11-27 16:48:41 +0000

    fix

--- FormalConjectures/ErdosProblems/1043.lean
+++ FormalConjectures/ErdosProblems/1043.lean
@@ -23,30 +23,40 @@ import FormalConjectures.Util.ProblemImports
 
 namespace Erdos1043
 
-open Complex MeasureTheory Set
+open Complex MeasureTheory Set Polynomial
 
 /-- The set $\{ z \in \mathbb{C} : \lvert f(z)\rvert\leq 1\}$ -/
-def LevelSet (f : Polynomial ℂ) : Set ℂ :=
+def levelSet (f : Polynomial ℂ) : Set ℂ :=
   {z : ℂ | ‖Polynomial.eval z f‖ ≤ 1}
 
 /--
-**Erdős Problem 1043**: Let $f\in \mathbb{C}[x]$ be a monic polynomial.
+**Erdős Problem 1043**:
+Let $f\in \mathbb{C}[x]$ be a monic polynomial.
 Must there exist a straight line $\ell$ such that the projection of
 \[\{ z: \lvert f(z)\rvert\leq 1\}\]
 onto $\ell$ has measure at most $2$?
 
-Note: A straight line $\ell$ can be parameterized as $\{t u \mid t \in \mathbb{R}\}$ for some
-unit vector $u \in \mathbb{C}$ (up to translation, which does not affect the measure of the projection).
-The projection onto this line, when mapped to $\mathbb{R}$, is given by $z \mapsto \operatorname{re}(\overline{u} z)$.
-We use `star u` for $\overline{u}$ which is the generic notation for conjugation on a star-ring.
-The measure on the line is the 1-dimensional Lebesgue measure, which is `volume` on $\mathbb{R}$.
-The problem is known to be solved (it is true, conjectured by Erdős and proved by Pommerenke in 1976).
+Pommerenke [Po61] proved that the answer is no.
+
+[Po61] Pommerenke, Ch., _On metric properties of complex polynomials._ Michigan Math. J. (1961),
+97-115.
 -/
 @[category research solved, AMS 28 30]
 theorem erdos_1043 :
-  ∀ (f : Polynomial ℂ), f.Monic →
-    ∃ (u : ℂ), ‖u‖ = 1 ∧
-    volume ((fun z : ℂ => (star u * z).re) '' (LevelSet f)) ≤ 2 :=
-by sorry
+    ∃ (f : ℂ[X]), f.Monic ∧
+      ∀ (u : ℂ), ‖u‖ = 1 ∧
+      volume ((ℝ ∙ u).orthogonalProjection '' levelSet f) > 2 := by
+  sorry
+
+/--
+On the other hand, Pommerenke also proved there always exists a line such that the projection has
+measure at most 3.3.
+-/
+@[category research solved, AMS 28 30]
+theorem erdos_1043.variants.weak :
+    ∀ (f : ℂ[X]), f.Monic ∧
+      ∃ (u : ℂ), ‖u‖ = 1 ∧
+      volume ((ℝ ∙ u).orthogonalProjection '' levelSet f) ≤ 3.3 := by
+  sorry
 
 end Erdos1043
