87184814412425d5a6829b842cdead26bf5fe121
Salvatore Mercuri <47568553+smmercuri@users.noreply.github.com>
2025-11-28 11:27:49 +0000

    Update FormalConjectures/ErdosProblems/695.lean
    Co-authored-by: Calle Sönne <csonne@google.com>

--- FormalConjectures/ErdosProblems/695.lean
+++ FormalConjectures/ErdosProblems/695.lean
@@ -55,6 +55,7 @@ theorem erdos_695.variant.upperBound :
       (∀ i, q (i + 1) % q i = 1) ∧
       ∃ o : ℕ → ℝ,
         (o =o[atTop] (1 : ℕ → ℝ)) ∧
+        -- We use `(k + 1)` here as the informal statement is 1-indexed.
         ∀ k, q k ≤ exp ((k + 1) * (log (k + 1)) ^ (1 + o k))) ↔
     answer(sorry) := by
   sorry
