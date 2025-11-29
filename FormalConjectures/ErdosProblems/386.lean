79403cc3d7d8259dfa02fb144f2260e78be37108
Marygold-Dusk <naranjosamantha1j@gmail.com>
2025-11-29 21:57:19 +0100

    Short version, no example

--- FormalConjectures/ErdosProblems/386.lean
+++ FormalConjectures/ErdosProblems/386.lean
@@ -15,7 +15,7 @@ limitations under the License.
 -/
 
 import FormalConjectures.Util.ProblemImports
-
+--open Nat
 /-!
 # Erdős Problem 386
 
@@ -26,39 +26,37 @@ import FormalConjectures.Util.ProblemImports
 namespace Erdos386
 
 /-
-This is the structure of the equation:
+S is a finite set of consecutive primes:
 -/
-
-def erdos_386_equation (a b : ℕ) (A : Finset ℕ) :=
-  Nat.choose a b = ∏ α ∈ A, α
-
+def ConsecutivePrimes (S : Finset ℕ) (l : ℕ) : Prop :=
+    ∃ a, Nat.card S = l ∧ S = {(a + n).nth Nat.Prime | (n : ℕ) (_ : n < l)}
 /-
-The following set contains all solutions `(n, k, P)` to the Erdős problem 386.
-A solution is a tuple `(n, k, P)`, where `n ≥ 2` and `k` are an integers and `P`
-is a non-empty finite set of
-distinct prime numbers, such that it's product is binomial coefficient n choose k.
+
+Erdos 386 Conjecture:
+Are there infinitely many integers n and k, such that the binomial coefficient of n and k is the
+product of consecutive prime numbers; i.e.,
+\[ {n}\choose{k} = \prod_{p \in P} p \] ?,
+where P is a set of consecutive prime numbers.
+
+
+A solution to Erdos' 386 is a tuple `(n, k, P)`, where `n` and `k` are integers and `P`
+is a non-empty finite set of distinct prime numbers, such that it's product is
+the binomial coefficient n choose k.
+Moreover `n` and `k` satisfy `2 ≤ n`, `2 ≤ k`, `k ≤ n - 2`.
 -/
 def erdos_386_solutions : Set (ℕ × ℕ × Finset ℕ) := {
   (n, k, P) |
-    (n ≥ 2 ∧ 2 ≤ k ∧ k ≤ n - 2) ∧
+    (2 ≤ n ∧ 2 ≤ k ∧ k ≤ n - 2) ∧
     P.Nonempty ∧
-    (∀ p ∈ P, p.Prime) ∧
-    erdos_386_equation n k P
+    ConsecutivePrimes P P.card ∧
+    Nat.choose n k = ∏ p ∈ P, p
   }
 /--
-Erdos 386 Conjecture:
-Are there infinitely many tuples `(n, k, P)`, where `n` and `k` are integers satisfying
-`n ≥ 2`, `2 ≤ k ≤ n - 2` and `P` is a set of distinct primes such that the following equation holds:
-\[ {n}\choose{k} = \prod_{p \in P} p \] ?
+
+Here is the formalisation of Erdos 386 problem:
 -/
 @[category research open, AMS 11]
 theorem erdos_386_conjecture : erdos_386_solutions.Infinite ↔ answer(sorry) := by
   sorry
-
-@[category test, AMS 11]
-theorem erdos_386_example :
-  (21, 2, ({2, 3, 5, 7} : Finset ℕ)) ∈ erdos_386_solutions := by
-  simp [erdos_386_solutions, erdos_386_equation, Nat.choose]
-  decide
 
 end Erdos386
