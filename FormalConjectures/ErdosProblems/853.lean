5e79dba3881a4dfa9a223cec607f8edc98096881
smmercuri <mercuris@google.com>
2025-12-04 15:51:48 +0000

    Exclude t = 0 from defn of r

--- FormalConjectures/ErdosProblems/853.lean
+++ FormalConjectures/ErdosProblems/853.lean
@@ -22,6 +22,8 @@ import FormalConjectures.Util.ProblemImports
 *Reference:* [erdosproblems.com/853](https://www.erdosproblems.com/853)
 -/
 
+open Filter
+
 namespace Erdos853
 
 /-
@@ -29,11 +31,7 @@ Let `r(x)` be the smallest even integer `t` such that
 `primeGap = t` has no solutions for `n ≤ x`.
 -/
 noncomputable def r (x : ℕ) : ℕ :=
-  sInf { t : ℕ | t % 2 = 0 ∧ ¬ (∃ n ≤ x, primeGap n = t) }
-
-@[category test, AMS 11]
-theorem r_0 : r 0 = 0 := by
-  simp [r, primeGap]
+  sInf { t : ℕ | 0 < t ∧ t % 2 = 0 ∧ ¬ (∃ n ≤ x, primeGap n = t) }
 
 /--
 Let $d_n = p_{n+1} - p_n$, where $p_n$ is the $n$th prime. Let $r(x)$ be the smallest even
@@ -43,7 +41,7 @@ Is it true that $r(x) \to \infty$?
 -/
 @[category research open, AMS 11]
 theorem erdos_853 :
-    ∀ M > 0, ∃ (X : ℕ), ∀ x ≥ X, r x > M := by
+    atTop.Tendsto r atTop := by
   sorry
 
 /--
@@ -53,7 +51,7 @@ integer $t$ such that $d_n = t$ has no solutions for $n \le x$.
 Is it true that $r(x) / \log x \to \infty$? -/
 @[category research open, AMS 11]
 theorem erdos_853_strong :
-    ∀ M > 0, ∃ (X : ℕ), ∀ x ≥ X, r x / Real.log (x) > M := by
+    atTop.Tendsto (fun n ↦ r n / Real.log n) atTop := by
   sorry
 
 end Erdos853
