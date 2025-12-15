85fa4e3158c441ad6c97cb3ff5d54394123756e8
Malhar A. Patel <malharalpeshpatel@gmail.com>
2025-12-15 21:03:29 +0000

    Add reference to docstring of `erdos_82.variants.F_upper_bound`

--- FormalConjectures/ErdosProblems/82.lean
+++ FormalConjectures/ErdosProblems/82.lean
@@ -51,6 +51,10 @@ theorem erdos_82 : Tendsto (fun n => F n / Real.log n) atTop atTop := by
 
 /--
 $F(n) \le O(n^{1/2} \ln ^ {3/4} n)$
+
+Theorem 1.4 from [AKS07]
+
+[AKS07] Alon, N. and Krivelevich, M. and Sudakov, B., Large nearly regular induced subgraphs. arXiv:0710.2106 (2007).
 -/
 @[category research solved, AMS 5]
 theorem erdos_82.variants.F_upper_bound :
