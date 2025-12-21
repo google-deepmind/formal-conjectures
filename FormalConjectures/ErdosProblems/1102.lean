49fce3ac86a015c78122adbf9640b76169c148c3
shamEiNew <panchalshamsundar7@gmail.com>
2025-12-21 19:46:02 +0530

    feat erdos-1102

---
+++ FormalConjectures/ErdosProblems/1102.lean
@@ -0,0 +1,71 @@
+/-
+Copyright 2025 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+import FormalConjectures.Util.ProblemImports
+
+open Squarefree
+
+/-!
+# Erdős Problem 1102
+
+*Reference:* [erdosproblems.com/1102](https://www.erdosproblems.com/1102)
+-/
+
+namespace Erdos1102
+
+/--
+Property P : A set $A ⊆ ℕ $ has property P, if for all $n ≥ 1$ the set
+$ \{a ∈ A | n + a is squarafree  \}$.
+-/
+
+def HasPropertyP (A : Set ℕ) : Prop :=
+  ∀ n ≥ 1, {a ∈ A | Squarefree (n + a)}.Finite
+
+/--
+Property Q : A set $A ⊆ ℕ $ has property Q, if for all $n ≥ 1$ the set
+$\{a ∈ ℕ  | ∀ a ∈ A, n > a and n + a is squarafree  \}$.
+-/
+
+def HasPropertyQ (A : Set ℕ): Prop :=
+  {n : ℕ | ∀ a ∈ A, a < n →  Squarefree (n + a)}.Infinite
+
+def IsIncreasing (u : ℕ → ℕ ) : Prop :=
+  ∀ n : ℕ, u n ≤ u (n + 1)
+
+def Aset (A : ℕ → ℕ) : Set ℕ :=
+  {x : ℕ | ∃ n, A n = x}
+
+/--
+Given a strictly increasing sequence `A : ℕ → ℕ` with `P` (resp. `Q`),
+characterize lower bounds on its growth, i.e. find explicit functions `f` such that
+for all sufficiently large `n`, we have `f n ≤ A n`.
+
+The problem seems to be solved based on this recent article by Terence Tao and
+Wouter Van Doorn in this [paper](https://arxiv.org/pdf/2512.01087)
+
+-/
+
+@[category research solved, AMS 11]
+theorem erdos_1102.HasPropertyP (A : ℕ → ℕ ): ∀ A : ℕ → ℕ, ∃ f : ℕ → ℝ,
+  IsIncreasing A → HasPropertyP (Aset A)  → ∃ N, ∀ n ≥ N, (A n : ℝ) ≥ f n := by
+  sorry
+
+@[category research solved, AMS 11]
+theorem erdos_1102.HasPropertyQ (A : ℕ → ℕ ) : ∀ A : ℕ → ℕ, ∃ (f : ℕ → ℝ),
+  IsIncreasing A → HasPropertyQ (Aset A)  → ∃ N, ∀ n ≥ N, (A n : ℝ) ≥ f n := by
+  sorry
+
+end Erdos1102
