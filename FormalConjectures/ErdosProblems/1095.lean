a89e900db187a1297e4467c006a01f2d5009f675
jeinryu <61902420+jeinryu@users.noreply.github.com>
2025-12-27 17:19:11 +0900

    feat(ErdosProblems): 1095

--- FormalConjectures/ErdosProblems/1095.lean
+++ FormalConjectures/ErdosProblems/1095.lean
@@ -24,43 +24,51 @@ import FormalConjectures.Util.ProblemImports
 
 namespace Erdos1095
 
-open Nat
+open Real Nat Filter Asymptotics
 
-/-
+/--
 Let $g(k)>k+1$ be maximal such that
 if $n\leq g(k)$ then $\binom{n}{k}$ is divisible by a prime $\leq k$.
 Estimate $g(k)$.
 -/
+noncomputable def g (k : ℕ) : ℕ :=
+  sSup {m | ∀ n : ℕ, (k < n ∧ n ≤ m) → (∃ p, p.Prime ∧ p ≤ k ∧ (p ∣ choose n k))}
 
-noncomputable def erdos_1095.g (k : ℕ) : ℕ :=
-  sSup {m | ∀ n : ℕ, k < n ∧ n ≤ m → (∃ p, p.Prime ∧ p ≤ k ∧ p | choose n k)}
 
-
-/-
+/--
 The current record is\[g(k) \gg \exp(c(\log k)^2)\]for some $c>0$,
 due to Konyagin [Ko99b](https://londmathsoc.onlinelibrary.wiley.com/doi/abs/10.1112/S0025579300007555).
 -/
-@[category research solved, AMS]
-theorem erdos_1095.lower_solved :
-    ∃ c > 0, ∀ (k : Nat) erdos_1095.g k ≥ exp (c * (log k)^2) := by
+@[category research solved, AMS 05 11]
+theorem erdos_1095_lower_solved :
+    ∃ c > 0, (fun k : ℕ => exp (c * (log k : ℝ)^2)) =O[atTop] fun k => (g k: ℝ) := by
   sorry
 
-/-
+/--
 Ecklund, Erdős, and Selfridge conjectured $g(k)\leq \exp(k^{1+o(1)})$
 [EES74](https://mathscinet.ams.org/mathscinet/relay-station?mr=1199990)
 -/
-@[category research open, AMS]
-theorem erdos_1095.upper_conjecture :
-    ∃ c ≥ 1, ∀ (k : Nat) erdos_1095.g k ≤ exp (k ^ c) := by
+@[category research open, AMS 05 11]
+theorem erdos_1095_upper_conjecture :
+    ∃ c ≥ 1, ∀ k : Nat, g k ≤ exp ((k : ℝ) ^ c) := by
   sorry
 
-/-
+/--
 Erdős, Lacampagne, and Selfridge [ELS93](https://www.ams.org/journals/mcom/1993-61-203/S0025-5718-1993-1199990-6/S0025-5718-1993-1199990-6.pdf)
 write 'it is clear to every right-thinking person' that
 $g(k)\geq\exp(c\frac{k}{\log k})$ for some constant $c>0$.
 -/
-@[category research open, AMS]
-theorem erdos_1095.lower_conjecture :
-    ∃ c > 0, ∀ (k : Nat) erdos_1095.g k ≥ exp (c * (k / log k)) := by
+@[category research open, AMS 05 11]
+theorem erdos_1095_lower_conjecture :
+    ∃ c > 0, ∀ k : Nat, g k ≥ exp (c * (k : ℝ) / log (k : ℝ)) := by
   sorry
+
+/--
+[Sorenson, Sorenson, and Webster](https://mathscinet.ams.org/mathscinet/relay-station?mr=4235124) give heuristic evidence that
+\[\log g(k) \asymp \frac{k}{\log k}.\]
+-/
+@[category research open, AMS 05 11]
+theorem erdos_1095_theta :
+    (fun k => log (g k : ℝ)) =Θ[atTop] (fun k => (k : ℝ) / log (k : ℝ)) :=
+sorry
 end Erdos1095
