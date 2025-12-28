7b2b5cd8504f92f1b83a7960c7052d1524ea7a93
Rmal <97214596+CoolRmal@users.noreply.github.com>
2025-12-27 22:23:02 -0500

    Update 517.lean

--- FormalConjectures/ErdosProblems/517.lean
+++ FormalConjectures/ErdosProblems/517.lean
@@ -30,29 +30,40 @@ import FormalConjectures.Util.ProblemImports
 open Set Filter
 
 /-- This is the terminology adopted in [Wa01] and some other sources. -/
-def hasFejerGaps (n : ℕ → ℕ) : Prop := StrictMono n ∧ Tendsto (fun k => n k / (k : ℝ)) atTop atTop
+def hasFabryGaps (n : ℕ → ℕ) : Prop := StrictMono n ∧ Tendsto (fun k => n k / (k : ℝ)) atTop atTop
 
-def hasFabryGaps (n : ℕ → ℕ) : Prop := StrictMono n ∧ Summable (fun k => 1 / (n k : ℝ))
+def hasFejerGaps (n : ℕ → ℕ) : Prop := StrictMono n ∧ Summable (fun k => 1 / (n k : ℝ))
 
 @[category API, AMS 40]
-theorem hasFabryGaps.hasFejerGaps {n : ℕ → ℕ} (hn : hasFabryGaps n) : hasFejerGaps n := by
+theorem hasFejerGaps.hasFabryGaps {n : ℕ → ℕ} (hn : hasFejerGaps n) : hasFabryGaps n := by
   refine ⟨hn.1, ?_⟩
-  by_contra! h
-  suffices ¬ Summable (fun k => 1 / (n k : ℝ)) from this hn.2
-  sorry
+  simp only [tendsto_atTop, eventually_atTop, ge_iff_le]
+  intro b
+  /- use the Cauchy criterion of series. -/
+  have : ∃ k, ∀ m ≥ k, ∑' (j : ℕ), 1 / (n (j + ⌊m / 2⌋₊) : ℝ) ≤ 1 / b := by sorry
+  obtain ⟨k, hk⟩ := this
+  refine ⟨k, fun m hm => ?_⟩
+  suffices m / n m ≤ 1 / b from by sorry
+  calc
+  _ = ∑ i ∈ Finset.range m, 1 / (n m : ℝ) := by sorry
+  _ ≤ ∑' (j : ℕ), 1 / (n (j + ⌊m / 2⌋₊) : ℝ) := by sorry
+  _ ≤ 1 / b := hk m hm
 
 namespace Erdos517
 
 /-- If `f(z) = ∑ aₖzⁿₖ` is an entire function such that `nₖ / k → ∞`, is it true that `f` assumes
 every value infinitely often? -/
 @[category research open, AMS 30]
-theorem erdos_517.fabry {f : ℂ → ℂ} (z : ℂ) : {x : ℂ | f x = z}.Infinite := by
+theorem erdos_517.fabry {f : ℂ → ℂ} {n : ℕ → ℕ} (hn : hasFabryGaps n) {a : ℕ → ℂ}
+    (hf : ∀ z, HasSum (fun k => (a k) * z ^ (n k)) (f z)) (z : ℂ) :
+    {x : ℂ | f x = z}.Infinite := by
   sorry
 
 /-- If `f(z) = ∑ aₖzⁿₖ` is an entire function such that `∑ 1 / nₖ < ∞`, then `f` assumes every value
 infinitely often. This theorem is proved in [Bi28]. -/
 @[category research solved, AMS 30]
-theorem erdos_517.fejer {f : ℂ → ℂ} (z : ℂ) : {x : ℂ | f x = z}.Infinite := by
+theorem erdos_517.fejer {f : ℂ → ℂ} {n : ℕ → ℕ} (hn : hasFejerGaps n) {a : ℕ → ℂ}
+    (hf : ∀ z, HasSum (fun k => (a k) * z ^ (n k)) (f z)) (z : ℂ) : {x : ℂ | f x = z}.Infinite := by
   sorry
 
 end Erdos517
