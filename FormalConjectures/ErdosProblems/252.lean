d14642145860664a026849e8f65979b1db49af10
Rmal <97214596+CoolRmal@users.noreply.github.com>
2025-12-31 14:02:08 -0500

    Update 252.lean

--- FormalConjectures/ErdosProblems/252.lean
+++ FormalConjectures/ErdosProblems/252.lean
@@ -19,20 +19,60 @@ import FormalConjectures.Util.ProblemImports
 /-!
 # Erdős Problem 252
 
-*Reference:* [erdosproblems.com/252](https://www.erdosproblems.com/252)
+*References:*
+ - [erdosproblems.com/252](https://www.erdosproblems.com/252)
+ - [ErSt71] Erdös, P., and E. G. Straus. "Some number theoretic results." Pacific J. Math 36 (1971):
+    635-646.
+ - [ErSt74] Erdős, Paul, and Ernst Straus. "On the irrationality of certain series." Pacific journal
+    of mathematics 55.1 (1974): 85-92.
+ - [ErKa54] P. Erdős, M. Kac, Amer. Math. Monthly 61 (1954), Problem 4518.
+ - [ScPu06] Schlage-Puchta, J. C., The irrationality of a number theoretical series. Ramanujan J.
+    (2006), 455-460.
+ - [FLC07] Friedlander, J. B. and Luca, F. and Stoiciu, M., On the irrationality of a divisor
+    function series. Integers (2007).
+ - [Pr22] Pratt, K., The irrationality of a divisor function series of Erdős and Kac.
+    arXiv:2209.11124 (2022).
 -/
 
-open Nat
+open scoped Nat
 
 namespace Erdos252
 
-def σ (k : ℕ) (n : ℕ) : ℕ := ∑ d : {d : Fin (n + 1) // d | n}
+def σ (k : ℕ) (n : ℕ) : ℕ := ∑ d : {d : Fin (n + 1) // d.1 ∣ n}, d ^ k
+
+/-- `∑ σ 0 n / n!` is irrational. `σ 0 n` is just the number of divisors of `n`, and this is proved
+in [ErSt71]. -/
+@[category research solved, AMS 11]
+theorem erdos_252_0 : Irrational (∑' n : ℕ, σ 0 n / (n ! : ℝ)) := by
+  sorry
+
+/-- `∑ σ 1 n / n!` is irrational. This is proved in [ErSt74]. -/
+@[category research solved, AMS 11]
+theorem erdos_252_1 : Irrational (∑' n : ℕ, σ 1 n / (n ! : ℝ)) := by
+  sorry
+
+
+/-- `∑ σ 2 n / n!` is irrational. This is proved in [ErKa54]. -/
+@[category research solved, AMS 11]
+theorem erdos_252_2 : Irrational (∑' n : ℕ, σ 2 n / (n ! : ℝ)) := by
+  sorry
+
+/-- `∑ σ 3 n / n!` is irrational. This is proved in [ScPu06] and [FLC07]. -/
+@[category research solved, AMS 11]
+theorem erdos_252_3 : Irrational (∑' n : ℕ, σ 3 n / (n ! : ℝ)) := by
+  sorry
+
+/-- `∑ σ 4 n / n!` is irrational. This is proved in [Pr22]. -/
+@[category research solved, AMS 11]
+theorem erdos_252_4 : Irrational (∑' n : ℕ, σ 4 n / (n ! : ℝ)) := by
+  sorry
 
 /--
-Is $\sum_{n=1}^\infty \frac{p_n}{2^n}$ irrational? Here $p_n$ is the $n$-th prime ($p_1=2, p_2=3, \dots$).
+For a fixed `k ≥ 5`, is `∑ σ k n / n!` irrational?.
 -/
 @[category research open, AMS 11]
-theorem erdos_252 : Irrational (∑' n : ℕ, (Nat.nth Nat.Prime n) / (2 ^ n)) ↔ answer(sorry) := by
+theorem erdos_252_ge_5 {k : ℕ} (hk : k ≥ 5) :
+    Irrational (∑' n : ℕ, σ k n / (n ! : ℝ)) ↔ answer(sorry) := by
   sorry
 
 end Erdos252
