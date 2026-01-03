a275486eb60a861fefce27d288d127311d75bd16
Rmal <97214596+CoolRmal@users.noreply.github.com>
2026-01-03 02:33:35 -0500

    Update 158.lean

--- FormalConjectures/ErdosProblems/158.lean
+++ FormalConjectures/ErdosProblems/158.lean
@@ -44,13 +44,13 @@ namespace Erdos158
 /-- Let `A` be an infinite `B₂[2]` set. Must `liminf |A ∩ {1, ..., N}|/√N = 0`? -/
 @[category research open, AMS 5 11]
 theorem erdos_158.B22 : answer(sorry) ↔ ∀ A : Set ℕ, A.Infinite → B2 2 A →
-    liminf (fun N : ℕ => (A ∩ (Finset.range N)).ncard / √(N : ℝ)) atTop = 0 := by
+    liminf (fun N : ℕ => (A ∩ (Finset.range N)).ncard / √N) atTop = 0 := by
   sorry
 
 /-- Let `A` be an infinite Sidon set. Then `liminf |A ∩ {1, ..., N}| * (log N / N) ^ (1 / 2) < ∞`.
 This is proved in [ESS94]. -/
 @[category research solved, AMS 11]
-theorem erdos_158.isSidon' {A : Set ℕ} (hp : A.Infinite) (hq : B2 2 A) :
+theorem erdos_158.isSidon' {A : Set ℕ} (hp : A.Infinite) (hq : IsSidon A) :
     liminf (fun N : ℕ => (A ∩ (Finset.range N)).ncard * ENNReal.ofReal (√(Real.log N / N)))
     atTop < ⊤ := by
   sorry
@@ -58,24 +58,22 @@ theorem erdos_158.isSidon' {A : Set ℕ} (hp : A.Infinite) (hq : B2 2 A) :
 /-- As a corollary of `erdos_158.isSidon'`, we can prove that `liminf |A ∩ {1, ..., N}|/√N = 0` for
 any infinite Sidon set `A`. -/
 @[category research solved, AMS 11]
-theorem erdos_158.isSidon {A : Set ℕ} (hp : A.Infinite) (hq : B2 2 A) :
+theorem erdos_158.isSidon {A : Set ℕ} (hp : A.Infinite) (hq : IsSidon A) :
     liminf (fun N : ℕ => (A ∩ (Finset.range N)).ncard / √(N : ℝ)) atTop = 0 := by
-  contrapose! hA with h_contra;
-  -- Since $\sqrt{\log N}$ grows to infinity, if the finite Minkowski dimension is not zero, then the number of elements in $A$ up to $N$ must grow at least logarithmically.
-  have h_log_growth : Filter.Tendsto (fun N => ((A ∩ (Finset.range N)).ncard : ℝ) * Real.sqrt (Real.log N / N)) Filter.atTop Filter.atTop := by
-    have h_log_growth : Filter.Tendsto (fun N => ((A ∩ (Finset.range N)).ncard : ℝ) / Real.sqrt N * Real.sqrt (Real.log N)) Filter.atTop Filter.atTop := by
-      have h_prod_inf : ∃ c > 0, ∀ᶠ N in Filter.atTop, ((Set.ncard (A ∩ (Finset.range N))) : ℝ) / Real.sqrt N ≥ c := by
-        simp_all +decide [ Filter.liminf_eq ];
-        by_cases h_zero : ∀ a ∈ {a : ℝ | ∃ a_1 : ℕ, ∀ b : ℕ, a_1 ≤ b → a ≤ (A ∩ Set.Iio b).ncard / Real.sqrt b}, a ≤ 0;
-        · exact False.elim <| h_contra <| le_antisymm ( csSup_le ⟨ 0, ⟨ 0, fun n hn => by positivity ⟩ ⟩ h_zero ) <| le_csSup ⟨ 0, h_zero ⟩ ⟨ 0, fun n hn => by positivity ⟩;
+  have := erdos_158.isSidon' hp hq
+  contrapose! this with h
+  have hg : Tendsto (fun N => (A ∩ (Finset.range N)).ncard * √(Real.log N / N)) atTop atTop := by
+    have hl : Tendsto (fun N => (A ∩ (Finset.range N)).ncard / √N * √(Real.log N)) atTop atTop := by
+      obtain ⟨c, hc_pos, hc⟩ : ∃ c > 0, ∀ᶠ N in atTop, (A ∩ (Finset.range N)).ncard / √N ≥ c := by
+        simp only [liminf_eq, eventually_atTop, ne_eq] at h
+        by_cases h_zero : ∀ a ∈ {a : ℝ | ∃ a_1 : ℕ, ∀ N : ℕ, a_1 ≤ N → a ≤ (A ∩ (Finset.range N)).ncard / √N}, a ≤ 0
+        · exact False.elim <| h <| le_antisymm ( csSup_le ⟨ 0, ⟨ 0, fun n hn => by positivity ⟩ ⟩ h_zero ) <| le_csSup ⟨ 0, h_zero ⟩ ⟨ 0, fun n hn => by positivity ⟩;
         · aesop;
-      obtain ⟨c, hc_pos, hc⟩ : ∃ c > 0, ∀ᶠ N in Filter.atTop, ((Set.ncard (A ∩ (Finset.range N))) : ℝ) / Real.sqrt N ≥ c := h_prod_inf;
-      refine' Filter.tendsto_atTop_mono' _ _ _;
-      exacts [ fun N => c * Real.sqrt ( Real.log N ), by filter_upwards [ hc, Filter.eventually_gt_atTop 1 ] with N hN₁ hN₂ using mul_le_mul_of_nonneg_right hN₁ <| Real.sqrt_nonneg _, Filter.Tendsto.const_mul_atTop hc_pos <| Filter.tendsto_atTop_atTop.mpr fun x => ⟨ ⌈Real.exp ( x ^ 2 ) ⌉₊ + 1, fun N hN => Real.le_sqrt_of_sq_le <| Real.le_log_iff_exp_le ( by norm_cast; linarith ) |>.2 <| by linarith [ Nat.le_ceil ( Real.exp ( x ^ 2 ) ), show ( N : ℝ ) ≥ ⌈Real.exp ( x ^ 2 ) ⌉₊ + 1 by exact_mod_cast hN ] ⟩ ];
-    convert h_log_growth using 2 ; norm_num [ div_eq_mul_inv, mul_assoc, mul_comm, mul_left_comm, Real.sqrt_mul ];
-  -- Since the cardinality times the square root of log N / N grows to infinity, the liminf must also be infinity.
-  have h_liminf_inf : Filter.Tendsto (fun N => ENNReal.ofReal ((A ∩ (Finset.range N)).ncard * Real.sqrt (Real.log N / N))) Filter.atTop (nhds ⊤) := by
-    exact ENNReal.tendsto_ofReal_atTop.comp h_log_growth;
-  rw [ Filter.Tendsto.liminf_eq ] ; aesop;
+      refine' tendsto_atTop_mono' _ _ _
+      exacts [ fun N => c * Real.sqrt ( Real.log N ), by filter_upwards [ hc, Filter.eventually_gt_atTop 1 ] with N hN₁ hN₂ using mul_le_mul_of_nonneg_right hN₁ <| Real.sqrt_nonneg _, Filter.Tendsto.const_mul_atTop hc_pos <| Filter.tendsto_atTop_atTop.mpr fun x => ⟨ ⌈Real.exp ( x ^ 2 ) ⌉₊ + 1, fun N hN => Real.le_sqrt_of_sq_le <| Real.le_log_iff_exp_le ( by norm_cast; linarith ) |>.2 <| by linarith [ Nat.le_ceil ( Real.exp ( x ^ 2 ) ), show ( N : ℝ ) ≥ ⌈Real.exp ( x ^ 2 ) ⌉₊ + 1 by exact_mod_cast hN ] ⟩ ]
+    convert hl using 1
+    norm_num [div_eq_mul_inv, mul_assoc, mul_comm, mul_left_comm]
+  rw [Tendsto.liminf_eq]
+  simpa [ofReal_mul, Function.comp_def] using tendsto_ofReal_atTop.comp hg
 
 end Erdos158
