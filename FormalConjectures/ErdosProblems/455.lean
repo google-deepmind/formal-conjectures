0a628ed9a417a85ec8ad38e71d56f755c6063bf8
Rmal <97214596+CoolRmal@users.noreply.github.com>
2026-01-03 10:39:08 -0500

    Update 455.lean

--- FormalConjectures/ErdosProblems/455.lean
+++ FormalConjectures/ErdosProblems/455.lean
@@ -35,9 +35,12 @@ theorem erdos_455: answer(sorry) ↔ ∀ q : ℕ → ℕ, StrictMono q →
     Tendsto (fun n : ℕ => (q n : ℝ) / n ^ 2) atTop atTop :=
   sorry
 
-@[category research open, AMS 11]
-theorem erdos_455.liminf: ∀ q : ℕ → ℕ, (StrictMono q ∧
-    ∀ n, (q n).Prime ∧ q (n + 1) - q n ≥ q n - q (n - 1)) →
+/--  Let `q : ℕ → ℕ` be a strictly increasing sequence of primes such that
+`q (n + 1) - q n ≥ q n - q (n - 1)`. Then `liminf q n / (n ^ 2) > 0.352`, and this is proved in
+[Ri76]. -/
+@[category research solved, AMS 11]
+theorem erdos_455.liminf: ∀ q : ℕ → ℕ, StrictMono q →
+    (∀ n, (q n).Prime ∧ q (n + 1) - q n ≥ q n - q (n - 1)) →
     liminf (fun n : ℕ => (q n : ℝ) / n ^ 2) atTop > 0.352 :=
   sorry
 
