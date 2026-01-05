c0dd27cdd38927ad1da3fbefbd55ddee71e65fcd
Aditi <aerilsoni05@gmail.com>
2026-01-05 10:46:37 +0530

    copyright header added to file

--- FormalConjectures/Oeis/080170_051283.lean
+++ FormalConjectures/Oeis/080170_051283.lean
@@ -1,3 +1,19 @@
+/-
+Copyright 2025 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
 import FormalConjectures.Util.ProblemImports
 
 /-!
@@ -9,7 +25,7 @@ For an integer k ≥ 2, the following are equivalent:
 (1) The greatest common divisor of the binomial coefficients
     C(2k,k), C(3k,k),.....,C((k+1)k,k) is equal to 1.
 
-(2) Writing prime factorization of k as 
+(2) Writing prime factorization of k as
     k = ∏ pᵢ^{eᵢ}, and let
     P = max pᵢ^{eᵢ},
     one has k / P > P.
@@ -17,42 +33,42 @@ For an integer k ≥ 2, the following are equivalent:
 This conjecture asserts that the integers satisfying (1)
 are exactly those satisfying (2).
 
-*Reference:* 
+*Reference:*
 - [A080170](https://oeis.org/A080170)
 - [A051283](https://oeis.org/A051283)
 -/
 
 namespace OeisA080170
 
-/-- 
-List of binomial coefficients: C(2k,k), C(3k,k), ..., C((k+1)k,k) 
+/--
+List of binomial coefficients: C(2k,k), C(3k,k), ..., C((k+1)k,k)
 -/
 def binom_sequence (k : ℕ) : List ℕ :=
   (List.range k).map (fun i => Nat.choose ((i + 2) * k) k)
 
-/-- 
-Condition: gcd of the binomial sequence is 1 
+/--
+Condition: gcd of the binomial sequence is 1
 -/
 def gcd_condition (k : ℕ) : Prop :=
   (binom_sequence k).foldr Nat.gcd 0 = 1
 
-/-- 
-Maximum prime power in the factorization of k 
+/--
+Maximum prime power in the factorization of k
 -/
 def max_prime_power (k : ℕ) : ℕ :=
   k.factorization.sup (fun p e => p ^ e)
 
-/-- 
-Condition: k divided by the maximum prime power is greater than the max prime power 
+/--
+Condition: k divided by the maximum prime power is greater than the max prime power
 -/
 def max_prime_condition (k : ℕ) : Prop :=
   k / max_prime_power k > max_prime_power k
 
-/-- 
+/--
 Conjecture: The gcd condition is equivalent to the max prime power condition
 -/
 @[category research open, AMS 11]
-conjecture A080170_A051283_equiv (k : ℕ) (hk : 2 ≤ k) : 
+conjecture A080170_A051283_equiv (k : ℕ) (hk : 2 ≤ k) :
   gcd_condition k ↔ max_prime_condition k
 
 
