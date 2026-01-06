120b0055fb358e3630145d39628d899d5a6667dd
Moritz Firsching <firsching@google.com>
2026-01-06 20:47:37 +0100

    more variants

--- FormalConjectures/ErdosProblems/477.lean
+++ FormalConjectures/ErdosProblems/477.lean
@@ -24,8 +24,6 @@ import FormalConjectures.Util.ProblemImports
 
 namespace Erdos477
 
--- TODO(lezeau): add the other (families of) counterexamples plus variants
-
 /--
 Let $f = X ^ 2$ be a polynomial of degree at least $2$. Then there is not set
 $A$ such that every $z\in \mathbb{Z}$ has exactly one representation as
@@ -33,8 +31,48 @@ $z=a+f(n)$ for some $a\in A$ and $n > 0$
 -/
 @[category research solved, AMS 12]
 theorem erdos_477.variants.explicit_counterexample :
-    letI f := Polynomial.X ^ 2
-    ¬ ∃ A : Set ℤ, ∀ z, ∃! a ∈ A ×ˢ (f.eval '' {n | 0 < n}), z = a.1 + a.2 :=
+    letI f := X ^ 2
+    ¬ ∃ (A : Set ℤ), ∀ z, ∃! a ∈ A ×ˢ (f.eval '' {n | 0 < n}), z = a.1 + a.2 := by
+  sorry
+
+/--
+There is no such $A$ for any polynomial $f(x) = aX^2 + bX + c$, if $a | b$
+with $a \ne 0$ and $b \ne 0.
+This was found be AlphaProof for the specific instance $X^2 - X + 1$ and then generalised.
+ -/
+@[category research solved, AMS 12]
+theorem erdos_477.variants.strong_negation_degree_two_dvd_condition_b_ne_zero (a b c : ℤ) :
+    let f := a • X ^ 2 + b • X + C c
+    a ≠ 0 → b ≠ 0 → a ∣ b →
+    ¬ ∃ (A : Set ℤ), ∀ z, ∃! a ∈ A ×ˢ (f.eval '' {n | 0 < n}), z = a.1 + a.2 := by
+  sorry
+
+
+/--
+Probably there is no such $A$ for any polynomial $f$ of degree $2$.
+-/
+@[category research open, AMS 12]
+theorem erdos_477.variants.strong_negation_degree_eq_two (f : Polynomial ℤ) (hf₀ : 2 = f.degree) :
+    ¬ ∃ (A : Set ℤ), ∀ z, ∃! a ∈ A ×ˢ (f.eval '' {n | 0 < n}), z = a.1 + a.2 := by
+  sorry
+
+
+/--
+Probably there is no such $A$ for the polynomial $X^3$.
+-/
+@[category research open, AMS 12]
+theorem erdos_477.variants.strong_negation_pow_three :
+    letI f := X ^ 3
+    ¬ ∃ (A : Set ℤ), ∀ z, ∃! a ∈ A ×ˢ (f.eval '' {n | 0 < n}), z = a.1 + a.2 := by
+  sorry
+
+/--
+Probably there is no such $A$ for the polynomial $X^k$ for any $k \ge 0$.
+-/
+@[category research open, AMS 12]
+theorem erdos_477.variants.strong_negation_monomial (k : ℕ) (hk : 2 ≤ k):
+    letI f := X ^ k
+    ¬ ∃ (A : Set ℤ), ∀ z, ∃! a ∈ A ×ˢ (f.eval '' {n | 0 < n}), z = a.1 + a.2 := by
   sorry
 
 /--
