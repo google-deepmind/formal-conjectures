a42273b83985ca9f699dd33268e0bbc51fcdd076
Moritz Firsching <firsching@google.com>
2026-01-07 13:47:36 +0100

    Update FormalConjectures/ErdosProblems/859.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- FormalConjectures/ErdosProblems/859.lean
+++ FormalConjectures/ErdosProblems/859.lean
@@ -50,7 +50,7 @@ for some positive constants `c₁` and `c₂`.
 -/
 @[category research open, AMS 11]
 theorem erdos_859 :
-    ∃ c₁ > 0, ∃ c₂ > (0 : ℝ), ∃ d : ℕ → ℝ, (∀ t > 0, ((DivisorSumSet t).HasDensity (d t))) ∧
+    ∃ c₁ > 0, ∃ c₂ > (0 : ℝ), ∃ d : ℕ → ℝ, (∀ t > 0, (DivisorSumSet t).HasDensity (d t)) ∧
       (fun (t : ℕ) ↦ d t) ~[atTop] (fun t ↦ c₁ / Real.log t ^ c₂) := by
   sorry
 
