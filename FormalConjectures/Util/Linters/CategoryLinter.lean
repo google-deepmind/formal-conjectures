40df92c5af58a2a2592dfe7b2dd967cb5942db42
Paul Lezeau <lezeau@google.com>
2026-01-09 17:15:55 +0000

    Fix build issue

--- FormalConjectures/Util/Linters/CategoryLinter.lean
+++ FormalConjectures/Util/Linters/CategoryLinter.lean
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -/
 
-import FormalConjectures.Util.Attributes
+import FormalConjectures.Util.Attributes.Attributes
 import Mathlib.Tactic.Lemma
 
 
