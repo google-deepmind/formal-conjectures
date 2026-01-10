93842c9c27ea7a64c8ace1cb46d12e56a5dd2b82
Rmal <97214596+CoolRmal@users.noreply.github.com>
2026-01-10 00:34:35 -0500

    Update 828.lean

--- FormalConjectures/ErdosProblems/828.lean
+++ FormalConjectures/ErdosProblems/828.lean
@@ -41,7 +41,7 @@ When $n > 1$, Lehmer conjectured that $\phi(n) | n - 1$ if and only if $n$ is pr
 theorem erdos_828.variants.lehmer_conjecture : answer(sorry) ↔ ∀ n > 1, φ n ∣ n - 1 ↔ Prime n := by
   sorry
 /--
-It is an easy exercise to show that $\phi(n) | n$ if and only if $n = 0, 1$ and $n = 2^a 3^b$.
+It is an easy exercise to show that $\phi(n) | n$ if and only if $n = 0, 1$ or $n = 2^a 3^b$.
 -/
 @[category undergraduate, AMS 11]
 theorem erdos_828.variants.phi_dvd_self_iff_pow2_pow3 {n : ℕ} :
