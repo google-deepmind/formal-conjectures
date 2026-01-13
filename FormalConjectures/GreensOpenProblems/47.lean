8ddf30df578beea15df756a77e9971e5959eb14c
Aditi <aerilsoni05@gmail.com>
2026-01-13 18:34:09 +0530

    Ben Green's open problem 47

---
+++ FormalConjectures/GreensOpenProblems/47.lean
@@ -0,0 +1,54 @@
+/-
+Copyright 2025 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+import FormalConjectures.Util.ProblemImports
+
+/-!
+# Ben Green's Open Problem 47
+
+Inverse large sieve problem.
+
+Suppose that $A \subseteq \mathbf{N}$ satisfies $|A \pmod{p}| \le \frac{p + 1}{2}$ for all
+sufficiently large primes $p$. Then either:
+
+1. `A` is very small in a sense that $|A \cap [1, X]| \ll \frac{X^{1/2}}{(\log X)^{100}}$
+
+2. `A` is contained in the image of `ℤ` under some quadratic map `φ : ℚ → ℚ`.
+
+*Reference:*
+
+- [Ben Green's Open Problem 47](https://people.maths.ox.ac.uk/greenbj/papers/open-problems.pdf#problem.47)
+- B. J. Green and A. Harper, "Inverse questions for the large sieve", Geom. Funct. Anal. Vol.
+  24 (2014) 1167–1203
+- H. A. Helfgott and A. Venkatesh, "How small must ill-distributed sets be?" Analytic number
+  theory, 224–234, Cambridge Univ. Press, Cambridge, 2009.
+- M. N. Walsh, "The inverse sieve problem in high dimensions". Duke Math. J. 161 (2012), no.
+  10, 2001–2022.
+- M. N. Walsh, "The algebraicity of ill-distributed sets". Geom. Funct. Anal. 24 (2014), no. 3,
+  959–967.
+
+-/
+
+namespace Green47
+
+@[category research open, AMS 11]
+theorem green_47 (A : Finset ℕ) :
+    (∀ p : ℕ, 0 < p → (A.image (λ a => a % p)).card ≤ p + 1/2) →
+    ∃ C : ℝ, ∀ X : ℕ, (A.filter (λ a => a ≤ X)).card ≤ C * (X : ℝ)^(1/2) := by
+  sorry
+
+
+end Green47
