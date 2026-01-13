39127b5c58cf93b23fa1271405e59549e5ed9ec7
Viktorija Brikule <viktoriabrikule@gmail.com>
2026-01-13 21:14:40 +0000

    Update 260.lean
    -Changed import
    -Added the attribute
    -Changed to using Irrational

--- FormalConjectures/ErdosProblems/260.lean
+++ FormalConjectures/ErdosProblems/260.lean
@@ -14,30 +14,26 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -/
 
-import Mathlib.Analysis.SpecialFunctions.Pow.Real
+import FormalConjectures.Util.ProblemImports
+
+/-!
+# Erdős Problem 260
+
+*Reference:* [erdosproblems.com/260](https://www.erdosproblems.com/260)
+-/
 
 namespace Erdos260
 
 open Filter SummationFilter
 
-
-/- limit of an/n -> infinity -/
 def an_div_n_tendsto_infinity (an : ℕ → ℝ) : Prop :=
-/- so the 'function' an/n tends to positive infinity -/
-Tendsto (fun n => an n / (n : ℝ)) atTop atTop
+    Tendsto (fun n => an n / (n : ℝ)) atTop atTop
 
-/- this defines the infinite sum of the sequence an / (2 ^ (an))-/
 noncomputable def sum_of_sequence (an : ℕ → ℝ) : ℝ :=
 ∑'[conditional ℕ] n, (an n / 2 ^ (an n))
 
-/- check if a sum is rational -/
-def sum_is_rational (a : ℕ → ℝ) : Prop := ∃ r : ℚ, (sum_of_sequence a) = (r : ℝ)
-
-/- So a is a function that maps natural to reals this is a(0), a(1), ... a (n) -/
-/- then a(n)/n tendsto positive infinity -/
+@[category research open, AMS 11]
 theorem erdos_260 (a : ℕ → ℝ) (h : StrictMono a) (h2 : an_div_n_tendsto_infinity a) :
-/- then the sum of a(n)/ 2^(a(n)) is not rational -/
-¬ sum_is_rational a := by sorry
-
+    Irrational (sum_of_sequence a) := by sorry
 
 end Erdos260
