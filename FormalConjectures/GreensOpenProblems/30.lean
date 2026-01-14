c10f6c1183471deaa0caf86341b4f15df515d7eb
Akshay Mishra <akshaymis2302@gmail.com>
2026-01-14 22:54:23 +0530

    feat(GreensOpenProblems): add Green's Open Problem 30

---
+++ FormalConjectures/GreensOpenProblems/30.lean
@@ -0,0 +1,26 @@
+/- 
+Copyright 2026 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+-/
+
+import FormalConjectures.Util.ProblemImports
+
+/-!
+# Ben Green's Open Problem 30
+
+*Reference:* https://people.maths.ox.ac.uk/greenbj/papers/open-problems.pdf#problem.30
+-/
+
+namespace Green30
+
+@[category research open, AMS 5 11]
+theorem green_30 :
+  answer(sorry) := by
+  sorry
+
+end Green30
