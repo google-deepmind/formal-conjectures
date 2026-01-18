50ab3be831c2e468deb5e2be9e4427c6c4cf054b
Ralf Stephan <ralf@ark.in-berlin.de>
2026-01-18 16:21:33 +0100

    trim imports

--- FormalConjecturesForMathlib/Data/Nat/PerfectPower.lean
+++ FormalConjecturesForMathlib/Data/Nat/PerfectPower.lean
@@ -13,10 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 -/
-import Mathlib.Data.Nat.Factorization.Basic
-import Mathlib.Algebra.GCDMonoid.Finset
-import Mathlib.Algebra.GCDMonoid.Nat
-import Mathlib.Data.Nat.PrimeFin
 import Mathlib.Tactic
 
 namespace Nat
