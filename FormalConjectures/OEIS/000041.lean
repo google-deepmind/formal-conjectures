19ea48b8f22d9a8785e77728c6321ae231855275
Ralf Stephan <ralf@ark.in-berlin.de>
2026-01-18 15:42:14 +0100

    Update FormalConjectures/OEIS/000041.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- FormalConjectures/OEIS/000041.lean
+++ FormalConjectures/OEIS/000041.lean
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -/
 import FormalConjectures.Util.ProblemImports
-import FormalConjecturesForMathlib.Data.Nat.PerfectPower
 
 open Nat
 namespace OeisA000041
