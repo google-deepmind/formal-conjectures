fd190d1e945daad92aa19898b621c7f5693c7129
danielchin <danielchin93@gmail.com>
2026-01-20 06:34:08 -0800

    address comments

--- FormalConjectures/ErdosProblems/197.lean
+++ FormalConjectures/ErdosProblems/197.lean
@@ -32,7 +32,7 @@ Can $\mathbb{N}$ be partitioned into two sets, each of which can be permuted to 
 -/
 @[category research open, AMS 5]
 theorem erdos_197 :
-    answer(sorry) ↔ ∃ A B : Set ℕ, Disjoint A B ∧ A ∪ B = univ ∧
+    answer(sorry) ↔ ∃ A B : Set ℕ, IsCompl A B ∧
       (∃ f : ℕ ≃ A, ¬ HasMonotoneAP f 3) ∧
       (∃ g : ℕ ≃ B, ¬ HasMonotoneAP g 3) := by
   sorry
