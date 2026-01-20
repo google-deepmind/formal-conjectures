e3581e09c3a9fe7aad36594aaabb4abc15b11bad
Batixx <s59fpern@uni-bonn.de>
2026-01-20 19:38:52 +0100

    apply suggestions

--- FormalConjectures/Mathoverflow/235893.lean
+++ FormalConjectures/Mathoverflow/235893.lean
@@ -48,19 +48,20 @@ Does there exist a bijection $f : ℝ^n → ℝ^n$ such that $f$ is connected bu
 -/
 @[category research open, AMS 26 54]
 theorem mathoverflow_235893 (n : ℕ) :
-  ∃ (f : ℝ^n → ℝ^n) (hf : Function.Bijective f),
-    IsConnectedMap f ∧ ¬ IsConnectedMap (Equiv.ofBijective f hf).symm ↔ answer(sorry) := by
+    answer(sorry) ↔ ∃ (f : ℝ^n ≅ ℝ^n), IsConnectedMap f.hom ∧ ¬ IsConnectedMap f.symm.hom := by
   sorry
 
 /--
 There exists a connected bijection ℝ → ℝ^2 where the inverse is not connected,
-proven in [mathoverflow/260589](https://mathoverflow.net/questions/260589) by
+proven in [mathoverflow/260589](https://mathoverflow.net/questions/260589) by user
 [Gro-Tsen](https://mathoverflow.net/users/17064/gro-tsen).
 -/
 @[category research solved, AMS 26 54]
 theorem mathoverflow_260589 :
-  ∃ (f : ℝ → ℝ^2) (hf : Function.Bijective f),
-    IsConnectedMap f ∧ ¬ IsConnectedMap (Equiv.ofBijective f hf).symm := by
+  ∃ (f : ℝ ≅ ℝ^2),
+    IsConnectedMap f.hom ∧ ¬ IsConnectedMap f.symm.hom := by
   sorry
+
+--TODO: Add remarks from the mathoverflow post
 
 end Mathoverflow235893
