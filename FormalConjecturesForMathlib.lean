54173a42be129f529cdd2801d7a1dd9cdc086250
Yaël Dillies <yael.dillies@gmail.com>
2026-01-21 10:01:43 +0100

    fix(Wikipedia/WallSunSun): restrict `D` to fundamental discriminants Not all integers appear as discriminants of a quadratic field.

--- FormalConjecturesForMathlib.lean
+++ FormalConjecturesForMathlib.lean
@@ -1,18 +1,3 @@
-/-
-Copyright 2026 The Formal Conjectures Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
--/
 module
 
 public import FormalConjecturesForMathlib.Algebra.GCDMonoid.Finset
@@ -85,6 +70,7 @@ public import FormalConjecturesForMathlib.NumberTheory.Amicable
 public import FormalConjecturesForMathlib.NumberTheory.BeurlingPrimes
 public import FormalConjecturesForMathlib.NumberTheory.CoveringSystem
 public import FormalConjecturesForMathlib.NumberTheory.DirichletCharacter.Basic
+public import FormalConjecturesForMathlib.NumberTheory.FundamentalDiscr
 public import FormalConjecturesForMathlib.NumberTheory.Lacunary
 public import FormalConjecturesForMathlib.NumberTheory.LegendreSymbol.Basic
 public import FormalConjecturesForMathlib.NumberTheory.PracticalNumbers
