226fe91230eb8007bdb647ed47aa7e27de2d8762
Gregory Hess <gregory.w.hess@gmail.com>
2026-01-20 17:01:40 -0800

    docstrings

--- FormalConjectures/GreensOpenProblems/4.lean
+++ FormalConjectures/GreensOpenProblems/4.lean
@@ -22,23 +22,31 @@ import FormalConjectures.Util.ProblemImports
 -/
 
 namespace Green4
+/--A set is product-free if for some * operation, there are no elements in the set x,y,z such that x*y=z
+-/
 def ProdFree {M : Type*} [Monoid M] (S : Set M) : Prop := ∀ x ∈ S, ∀ y ∈ S, ∀ z ∈ S, x * y ≠ z
 
-/-- What is the largest product-free set in the alternating group A_n?-/
+/-- What is the largest product-free set in the alternating group $A_n$?-/
 @[category research open, AMS 20]
-theorem green_4 (n : ℕ) : 
-    MaximalFor (M := alternatingGroup <| Fin n) ProdFree Set.ncard answer(sorry) := by
+theorem green_4 (n : ℕ) :
+    MaximalFor (ProdFree (M := alternatingGroup <| Fin n)) Set.ncard answer(sorry) := by
   sorry
 
+/-- Defines a family of subsets of $A_n$ where each permutation π in a subset obeys π(x)∈I and ∀v∈I,
+π(v)∉I for fixed x and I. It is easy to demonstrate that such a subset is product-free, because for
+any a,b,c in such a set, (a*b) (x)=a(b(x))∉I but c(x)
+-/
 def extremalFamily {n : ℕ} (x : Fin n) (I : Set (Fin n)) : Set <| alternatingGroup <| Fin n :=
   {π | π.val x ∈ I ∧ Disjoint (I.image π) I}
 
 /-- In the case of large n, the problem was solved in
-[On the largest product-free subsets of the alternating groups](https://arxiv.org/pdf/2205.15191)
+[On the largest product-free subsets of the alternating groups](https://arxiv.org/pdf/2205.15191).
+Specifically, this theorem formalizes the statement of theorem 1.1 in the mentioned paper
 -/
 @[category research solved, AMS 20]
-theorem large_green_4: ∃N : ℕ, ∀n > N, ∀S, LargestProdFree n S → ∃x I, S = extremalFamily x I ∨
-    S = (extremalFamily x I).image (fun x=>x⁻¹) := by
+theorem large_green_4: ∀ᶠn in Filter.atTop,
+  ∀S, MaximalFor (ProdFree (M := alternatingGroup <| Fin n)) Set.ncard S →
+  ∃x I, S = extremalFamily x I ∨ S = (extremalFamily x I).image (·⁻¹) := by
   sorry
 
 end Green4
