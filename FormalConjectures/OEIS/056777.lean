c06e62cf79d93e428aba1715c9bb79a544fc289d
Ralf Stephan <ralf@ark.in-berlin.de>
2026-01-23 15:16:01 +0100

    feat(OEIS): A056777

---
+++ FormalConjectures/OEIS/056777.lean
@@ -0,0 +1,45 @@
+/-
+Copyright 2026 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+import FormalConjectures.Util.ProblemImports
+
+open scoped ArithmeticFunction
+
+/-!
+# **Conjecture: A056777**:
+If n is composite and satisfies both:
+- φ(n+12) = φ(n) + 12  (Euler's totient)
+- σ(n+12) = σ(n) + 12  (sum of divisors)
+
+then n ≡ 65 (mod 72).
+
+This conjecture, posed by R. Stephan, probably requires meticulous congruence case analysis
+of the φ and σ functions modulo 8 and 9.
+
+*References:*
+    * https://oeis.org/A056777
+-/
+
+namespace OeisA056777
+
+@[category undergraduate, AMS 11]
+theorem conjectureA056777 (n : ℕ)
+    (hcomp : ¬ n.Prime ∧ 1 < n)
+    (htot : Nat.totient (n + 12) = Nat.totient n + 12)
+    (hsig : σ 1 (n + 12) = σ 1 n + 12) :
+    n % 72 = 65 := by
+  sorry
+
+end OeisA056777
