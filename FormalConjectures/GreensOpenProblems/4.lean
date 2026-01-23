76b23367ca33a76ca9e7fd04dcaec7e5f5ddf036
Gregory Hess <gregory.w.hess@gmail.com>
2026-01-23 15:32:56 -0800

    Update FormalConjectures/GreensOpenProblems/4.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- FormalConjectures/GreensOpenProblems/4.lean
+++ FormalConjectures/GreensOpenProblems/4.lean
@@ -46,8 +46,8 @@ Specifically, this theorem formalizes the statement of theorem 1.1 in the mentio
 -/
 @[category research solved, AMS 20]
 theorem large_green_4 : ∀ᶠ n in .atTop,
-  ∀ S, MaximalFor (ProdFree (M := alternatingGroup <| Fin n)) Set.ncard S →
-  ∃ x I, S = extremalFamily x I ∨ S = (extremalFamily x I).image (·⁻¹) := by
+    ∀ S, MaximalFor (ProdFree (M := alternatingGroup <| Fin n)) Set.ncard S →
+    ∃ x I, S = extremalFamily x I ∨ S = (extremalFamily x I).image (·⁻¹) := by
   sorry
 
 end Green4
