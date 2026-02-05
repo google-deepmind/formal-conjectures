5b878fb91f066970c0653d7a962bc7b975634bb4
Yaël Dillies <yael.dillies@gmail.com>
2026-02-05 16:36:23 +0100

    typo

--- FormalConjecturesForMathlib/Topology/GDelta.lean
+++ FormalConjecturesForMathlib/Topology/GDelta.lean
@@ -22,7 +22,7 @@ A space where all singletons are Gδ sets.
 class HasGδSingletons (X : Type*) [TopologicalSpace X] : Prop where
   isGδ_singleton : ∀ ⦃x : X⦄, IsGδ {x}
 
-/-- Singletons are Gδ in First-Countable T₁ Spaces- -/
+/-- Singletons are Gδ in first-countable T₁ spaces. -/
 instance HasGδSingletons.of_t1Space_firstCountableTopology (X : Type*) [TopologicalSpace X]
     [FirstCountableTopology X] [T1Space X] : HasGδSingletons X where
   isGδ_singleton := IsGδ.singleton
