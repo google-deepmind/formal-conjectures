c2103f092dad6e81baf26ac18688ac66e2ab8614
danielchin <danielchin93@gmail.com>
2026-02-05 22:14:07 -0800

    1145 implies 28

--- FormalConjectures/ErdosProblems/1145.lean
+++ FormalConjectures/ErdosProblems/1145.lean
@@ -15,6 +15,7 @@ limitations under the License.
 -/
 
 import FormalConjectures.Util.ProblemImports
+import FormalConjectures.ErdosProblems.«28»
 
 /-!
 # Erdős Problem 1145
@@ -24,10 +25,16 @@ import FormalConjectures.Util.ProblemImports
 - [erdosproblems.com/1145](https://www.erdosproblems.com/1145)
 -/
 
-open Set Filter Pointwise Topology
+open Set Filter Pointwise Topology AdditiveCombinatorics
 
 namespace Erdos1145
 
+
+def Erdos1145Prop : Prop :=
+  ∀ ⦃A B : Set ℕ⦄ (_ : A.Infinite) (_ : B.Infinite),
+    Tendsto (fun n ↦ (Nat.nth (· ∈ A) n : ℝ) / (Nat.nth (· ∈ B) n : ℝ)) atTop (𝓝 1) →
+    (∀ᶠ n in atTop, n ∈ A + B) →
+    limsup (fun n => ↑(((𝟙_A ∗ 𝟙_B) : ℕ → ℕ) n)) atTop = (⊤ : ℕ∞)
 
 /--
 Let $A=\{1\leq a_1 < a_2 < \cdots\}$ and $B=\{1\leq b_1 < b_2 < \cdots\}$ be sets of integers with
@@ -37,17 +44,24 @@ If $A+B$ contains all sufficiently large positive integers then is it true that
 $\limsup 1_A\ast 1_B(n)=\infty$?
 
 A conjecture of Erdős and Sárközy.
-
-A stronger form of [erdosproblems.com/28].
 -/
 @[category research open, AMS 5]
-theorem erdos_1145 :
-  answer(sorry) ↔ ∀ ⦃A B : Set ℕ⦄ (hA : A.Infinite) (hB : B.Infinite),
-    0 ∉ A →
-    0 ∉ B →
-    Tendsto (fun n ↦ (Nat.nth (· ∈ A) n : ℝ) / (Nat.nth (· ∈ B) n : ℝ)) atTop (𝓝 1) →
-    (∀ᶠ n in atTop, n ∈ A + B) →
-    limsup (fun n => ↑(((𝟙_A ∗ 𝟙_B) : ℕ → ℕ) n)) atTop = (⊤ : ℕ∞) := by
+theorem erdos_1145 : answer(sorry) ↔ Erdos1145Prop := by
   sorry
+
+/--
+A stronger form of [erdosproblems.com/28].
+-/
+@[category test, AMS 11]
+theorem erdos_1145_implies_erdos_28 : Erdos1145Prop → type_of% Erdos28.erdos_28 := by
+  delta sumRep
+  use fun and K V=>V.exists_le.elim fun R L=>by_contra fun and' =>?_
+  by_cases h:K.Finite
+  · use(h.add h).infinite_compl V
+  · bound [and h h
+      (tendsto_atTop_of_eventually_const fun and p =>
+        div_self (mod_cast (ne_of_gt (p.trans ((Nat.nth_strictMono h)).le_apply))))
+      ((Filter.eventually_gt_atTop R).mono fun and =>
+        not_imp_comm.1 (L _) ∘ not_le_of_gt)]
 
 end Erdos1145
