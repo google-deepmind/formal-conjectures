b8a0da1251df3e8bf3746c86a452f7dec40b4da2
Paul Lezeau <lezeau@google.com>
2026-02-17 12:20:13 +0000

    Typos

--- FormalConjectures/Millenium/Poincarre.lean
+++ FormalConjectures/Millenium/Poincarre.lean
@@ -16,7 +16,7 @@ limitations under the License.
 
 import FormalConjectures.Util.ProblemImports
 
-/-! # The Poincarre Conjecture
+/-! # The Poincaré Conjecture
 
 References:
 - [Miln2022](https://www.claymath.org/wp-content/uploads/2022/06/poincare.pdf)
@@ -35,40 +35,40 @@ open ContinuousMap
 
 open EuclideanGeometry
 
-namespace PoincarreConjecture
+namespace PoincareConjecture
 
-/-- The predicate that the generalized Poincarre conjecture holds in dimension $n$, i.e. that
+/-- The predicate that the generalized Poincaré conjecture holds in dimension $n$, i.e. that
 any $n$-dimensional manifold that is homotopy equivalent to the sphere is in fact homeomorphic
 to the sphere. -/
 def ConjectureFor (n : ℕ) : Prop :=
   ∀ (M : Type) [TopologicalSpace M] [T2Space M] [ChartedSpace (ℝ^n) M], M ≃ₕ 𝕊ⁿ → Nonempty (M ≃ₜ 𝕊ⁿ)
 
 /--
-The Millenium Problem, solved by Grigori Perelman in 2003: the Poincarre Conjecture holds.
+The Millenium Problem, solved by Grigori Perelman in 2003: the Poincaré Conjecture holds.
 -/
 @[category research solved, AMS 54 57]
-theorem poincarre_conjecture : ConjectureFor 3 := by
+theorem poincare_conjecture : ConjectureFor 3 := by
   sorry
 
 /--
-The Generalized Poincarre Conjecture holds for surfaces.
+The Generalized Poincaré Conjecture holds for surfaces.
 -/
 @[category undergraduate, AMS 54 57]
 theorem poincarre_conjecture.variants.dimension_two : ConjectureFor 2 := by
   sorry
 
 /--
-The Generalized Poincarre Conjecture holds for dimensions at least 5.
+The Generalized Poincaré Conjecture holds for dimensions at least 5.
 -/
 @[category undergraduate, AMS 54 57]
-theorem poincarre_conjecture.variants.dimension_ge_five (n : ℕ) (hn : 5 ≤ n) : ConjectureFor n := by
+theorem poincare_conjecture.variants.dimension_ge_five (n : ℕ) (hn : 5 ≤ n) : ConjectureFor n := by
   sorry
 
 /--
-The Generalized Poincarre Conjecture holds in dimension 4.
+The Generalized Poincaré Conjecture holds in dimension 4.
 -/
 @[category undergraduate, AMS 54 57]
-theorem poincarre_conjecture.variants.dimension_four : ConjectureFor 4 := by
+theorem poincare_conjecture.variants.dimension_four : ConjectureFor 4 := by
   sorry
 
 /-- The predicate that the smooth Poincaré conjecture holds in dimension $n$. -/
@@ -78,37 +78,37 @@ def SmoothConjectureFor (n : ℕ) : Prop :=
 
 /-- A reformulation of the Millenium Problem in terms of smooth 3-folds. -/
 @[category undergraduate, AMS 54 57]
-theorem poincarre_conjecture.variants.smooth_for_three : SmoothConjectureFor 3 := by
+theorem poincare_conjecture.variants.smooth_for_three : SmoothConjectureFor 3 := by
   sorry
 
 /-- The smooth formulation of the Millenium Problem implies the general case. This follows from
 the fact that every topological 3-fold admits a smooth structure [mo296171]. -/
 @[category graduate, AMS 54 57]
-theorem poincarre_conjecture.variants.smooth_implication (H : SmoothConjectureFor 3) :
+theorem poincare_conjecture.variants.smooth_implication (H : SmoothConjectureFor 3) :
     ConjectureFor 3 := by
   sorry
 
 /-- The values at which the smooth version of the conjecture is known to hold. -/
 def SmoothTrueValues : Set ℕ := {1, 2, 3, 5, 6, 12, 56, 61}
 
-/-- The smooth version of the Poincarre conjecture is known to hold in dimensions
+/-- The smooth version of the Poincaré conjecture is known to hold in dimensions
 $1, 2, 3, 5, 6, 12, 56, 61$. See [Wang2017]. -/
 @[category research solved, AMS 54 57]
-theorem poincarre_conjecture.variants.smooth_known_cases (n : ℕ) (hn : n ∈ SmoothTrueValues) :
+theorem poincare_conjecture.variants.smooth_known_cases (n : ℕ) (hn : n ∈ SmoothTrueValues) :
     SmoothConjectureFor n := by
   sorry
 
 /-- The four dimensional case of the smooth version of the conjecture is still open.
 See [Wang2017]. -/
 @[category research open, AMS 54 57]
-theorem poincarre_conjecture.variants.smooth_dimension_four : SmoothConjectureFor 4 := by
+theorem poincare_conjecture.variants.smooth_dimension_four : SmoothConjectureFor 4 := by
   sorry
 
 /-- It is conjectured that the only values of $n > 4$ for which the smooth version of the
 conjecture holds are $n = 5, 6, 12, 56, 61$. See Conjecture 1.17 in [Wang2017]. -/
 @[category research open, AMS 54 57]
-theorem poincarre_conjecture.variants.smooth_other_cases (n : ℕ) (hn : n > 4)
+theorem poincare_conjecture.variants.smooth_other_cases (n : ℕ) (hn : n > 4)
     (hn' : n ∉ SmoothTrueValues) : ¬ SmoothConjectureFor n := by
   sorry
 
-end PoincarreConjecture
+end PoincareConjecture
