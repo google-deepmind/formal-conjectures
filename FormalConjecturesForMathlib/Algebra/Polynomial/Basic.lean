a6318bd09aefb2837a86729af6015df36f833660
Yaël Dillies <yael.dillies@gmail.com>
2026-02-18 09:10:23 +0100

    chore: bump mathlib to v4.28.0

--- FormalConjecturesForMathlib/Algebra/Polynomial/Basic.lean
+++ FormalConjecturesForMathlib/Algebra/Polynomial/Basic.lean
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 -/
-import Mathlib.Algebra.Polynomial.Degree.Definitions
+import Mathlib.Algebra.Polynomial.Degree.Defs
 import Mathlib.Algebra.Polynomial.Eval.Defs
 import Mathlib.Data.Nat.Prime.Defs
 
