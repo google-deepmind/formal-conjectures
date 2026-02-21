cd8451989ea96193c28e48bc822319c0f6fc7e68
Jean-Guillaume Durand <jeangud7@gmail.com>
2026-02-20 16:47:10 -0800

    Added known bounds. Needs to express open problem.

--- FormalConjectures/GreensOpenProblems/27.lean
+++ FormalConjectures/GreensOpenProblems/27.lean
@@ -21,10 +21,12 @@ import FormalConjectures.Util.ProblemImports
 
 References:
 - [Gr24] [Green, Ben. "100 open problems." (2024).](https://people.maths.ox.ac.uk/greenbj/papers/open-problems.pdf#problem.27)
+- [Be23] Bedert, Benjamin. "On unique sums in Abelian groups." Combinatorica 44.2 (2024): 269-298.
 -/
 
-namespace Green27
+open Filter Topology
 
+namespace Green27
 /--
 A set `A` has no unique representation in its sumset `A + A` if for every pair of elements
 `a₁, a₂ ∈ A`, there exist another pair of elements `b₁, b₂ ∈ A` such that `a₁ + a₂ = b₁ + b₂`
@@ -39,14 +41,46 @@ The size of the smallest set $A \subset \mathbb{Z} / p\mathbb{Z}$ (with at least
 for which no element in the sumset $A + A$ has a unique representation.
 -/
 noncomputable def minSizeNoUniqueRep (p : ℕ) [Fact p.Prime] : ℕ :=
-  sInf { k | ∃ A : Finset (ZMod p), A.card = k ∧ 2 ≤ k ∧ HasNoUniqueRepresentation A }
+  sInf { Finset.card A | (A : Finset (ZMod p)) (_ : 2 ≤ A.card) (_ : HasNoUniqueRepresentation A) }
 
 /--
 What is the size of the smallest set $A \subset \mathbb{Z} / p\mathbb{Z}$ (with at least two elements)
 for which no element in the sumset $A + A$ has a unique representation?
 -/
 @[category research open, AMS 5 11]
-theorem green_27 (p : ℕ) [Fact p.Prime] : minSizeNoUniqueRep p = answer(sorry) := by
+theorem green_27 (p : ℕ) [Fact p.Prime] :
+  minSizeNoUniqueRep p = answer(sorry) := by
+  sorry
+
+/--
+$m(p) \geq \omega(p) \log p$ for some function $\omega(p)$ tending to infinity [Be23, Theorem 3].
+-/
+@[category research solved, AMS 5 11]
+theorem green_27.variants.lower_be23 :
+  ∃ ω : ℕ → ℝ, Tendsto ω atTop atTop ∧
+    ∀ᶠ p in atTop, ∀ [Fact p.Prime],
+      ω p * Real.log (p : ℝ) ≤ (minSizeNoUniqueRep p : ℝ) := by
+  sorry
+
+/-- $m(p) \ll (\log p)^2$ [Be23, Theorem 5]. -/
+@[category research solved, AMS 5 11]
+theorem green_27.variants.upper_be23 :
+  ∃ C > 0, ∀ᶠ p in atTop, ∀ [Fact p.Prime],
+    (minSizeNoUniqueRep p : ℝ) ≤ C * (Real.log (p : ℝ)) ^ 2 := by
+  sorry
+
+/-- Previous best-known lower bound [Be23]. -/
+@[category research solved, AMS 5 11]
+theorem green_27.variants.previous_lower :
+  ∃ C > 0, ∀ᶠ p : ℕ in atTop, ∀ [Fact p.Prime],
+    C * Real.log (p : ℝ) ≤ (minSizeNoUniqueRep p : ℝ) := by
+  sorry
+
+/-- Previous best-known upper bound [Be23]. -/
+@[category research solved, AMS 5 11]
+theorem green_27.variants.previous_upper :
+  ∃ C > 0, ∀ᶠ p : ℕ in atTop, ∀ [Fact p.Prime],
+    (minSizeNoUniqueRep p : ℝ) ≤ C * Real.sqrt (p : ℝ) := by
   sorry
 
 end Green27
