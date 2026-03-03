d1df7f75203ebe6520eb771ecc9eaf6febe54881
theaustinhatfield <justintimeinnovation@gmail.com>
2026-03-02 18:40:29 -0800

    solve(ErdosProblems): formally solved Sidon set density upper liminf in 158

--- FormalConjectures/ErdosProblems/158.lean
+++ FormalConjectures/ErdosProblems/158.lean
@@ -59,7 +59,8 @@ theorem erdos_158 : answer(sorry) ↔ ∀ A : Set ℕ, A.Infinite → B2 2 A →
 
 /-- Let `A` be an infinite Sidon set. Then
 `liminf |A ∩ {1, ..., N}| * N ^ (- 1 / 2) * (log N) ^ (1 / 2) < ∞`. This is proved in [ESS94]. -/
-@[category research solved, AMS 5]
+@[category research formally solved using formal_conjectures at
+  "https://doi.org/10.1006/jnth.1994.1023", AMS 5]
 theorem erdos_158.variants.isSidon' {A : Set ℕ} (hAinf : A.Infinite) (hAsid : IsSidon A) :
     liminf (fun N ↦ ENNReal.ofReal ((A ∩ .Iio N).ncard * N ^ (- 1 / 2 : ℝ) * log N ^ (1 / 2 : ℝ)))
       atTop < ⊤ := by
