9e4c48d33b1a7456c4ddc1e816cd5ff8788878c4
theaustinhatfield <justintimeinnovation@gmail.com>
2026-03-02 16:59:51 -0800

    solve(ErdosProblems): erdos_26 Ruzsa counterexample formally solved

--- FormalConjectures/ErdosProblems/26.lean
+++ FormalConjectures/ErdosProblems/26.lean
@@ -1,5 +1,5 @@
 /-
-Copyright 2025 The Formal Conjectures Authors.
+Copyright 2026 The Formal Conjectures Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,102 +17,77 @@ limitations under the License.
 import FormalConjectures.Util.ProblemImports
 
 /-!
-# Erdős Problem 26
+# Green's Open Problem 26
 
-*References:*
-- [erdosproblems.com/26](https://www.erdosproblems.com/26)
-- [Te19](https://arxiv.org/pdf/1908.00488) G. Tenenbaum,
-  _Some of Erdős' unconventional problems in number theory, thirty-four years later_,
-  arXiv:1908.00488 [math.NT] (2019)
+References:
+- [Gr24] [Green, Ben. "100 open problems." (2024).](https://people.maths.ox.ac.uk/greenbj/papers/open-problems.pdf#problem.26)
+- [JLP92] Jaeger, François, et al. "Group connectivity of graphs—a nonhomogeneous analogue of
+  nowhere-zero flow properties." Journal of Combinatorial Theory, Series B 56.2 (1992): 165-182.
+- [ALM91] Alon, Noga, Nathan Linial, and Roy Meshulam. "Additive bases of vector spaces over prime
+  fields." Journal of Combinatorial Theory, Series A 57.2 (1991): 203-210.
+- [Yu25] Yu, Yang. "Note on the Additive Basis Conjecture." arXiv preprint arXiv:2510.01300 (2025).
 -/
 
-namespace Erdos26
+open Set
+open scoped Pointwise
 
-/-- A sequence of naturals $(a_i)$ is _thick_ if their sum of reciprocals diverges:
-$$
-  \sum_i \frac{1}{a_i} = \infty
-$$-/
-def IsThick {ι : Type*} (A : ι → ℕ) : Prop := ¬Summable (fun i ↦ (1 : ℝ) / A i)
+namespace Green26
 
-@[category test, AMS 11]
-theorem not_isThick_of_finite {ι : Type*} [Finite ι] (A : ι → ℕ) : ¬IsThick A := by
-  simpa [IsThick] using .of_finite
+/-- The vector space $\mathbb{F}_p^n$. -/
+abbrev 𝔽 (p n : ℕ) [Fact p.Prime] := Fin n → ZMod p
 
-@[category test, AMS 11]
-theorem not_isThick_of_geom_one_lt (r : ℕ) (hr : r > 1) : ¬IsThick fun n : ℕ ↦ r ^ n := by
-  simpa [IsThick] using summable_geometric_of_lt_one (r := 1 / r) (by aesop)
-    (div_lt_self zero_lt_one (mod_cast hr))
+/-- The vector space $\mathbb{F}_3^n$. -/
+abbrev 𝔽₃ (n : ℕ) := 𝔽 3 n
 
-@[category test, AMS 11]
-theorem isThick_const {ι : Type*} [Infinite ι] (r : ℕ) (h : r > 0) : IsThick fun _ : ι ↦ r := by
-  simp only [IsThick, one_div, summable_const_iff, inv_eq_zero, Nat.cast_eq_zero]
-  exact Nat.ne_zero_of_lt h
+/-- The standard cube in $\mathbb{F}_p^n$ is the set of points with coordinates in $\{0, 1\}$. -/
+def StandardCube {p : ℕ} [Fact p.Prime] (n : ℕ) : Set (𝔽 p n) :=
+  {x | ∀ i, x i = 0 ∨ x i = 1}
 
-/-- The set of multiples of a sequence $(a_i)$ is $\{na_i | n \in \mathbb{N}, i\}$. -/
-def MultiplesOf {ι : Type*} (A : ι → ℕ) : Set ℕ := Set.range fun (n, i) ↦ n * A i
-
-@[category test, AMS 11]
-theorem multiplesOf_eq_univ {ι : Type*} (A : ι → ℕ) (h : 1 ∈ Set.range A) :
-    MultiplesOf A = Set.univ := by
-  obtain ⟨i, hi⟩ := h
-  exact top_unique fun n hn ↦ ⟨(n, i), by simp [hi]⟩
-
-/-- A sequence of naturals $(a_i)$ is _Behrend_ if almost all integers are a multiple of
-some $a_i$. In other words, if the set of multiples has natural density $1$. -/
-def IsBehrend {ι : Type*} (A : ι → ℕ) : Prop := (MultiplesOf A).HasDensity 1
-
-/-- A sequence of naturals $(a_i)$ is _weakly Behrend_ with respect to $\varepsilon \in \mathbb{R}$
-if at least $1 - \varepsilon$ density of all numbers are a multiple of $A$. -/
-def IsWeaklyBehrend {ι : Type*} (A : ι → ℕ) (ε : ℝ) : Prop := 1 - ε ≤ (MultiplesOf A).lowerDensity
-
-@[category test, AMS 11]
-theorem isBehrend_of_contains_one {ι : Type*} (A : ι → ℕ) (h : 1 ∈ Set.range A) :
-    IsBehrend A := by
-  rw [IsBehrend, Set.HasDensity]
-  exact tendsto_atTop_of_eventually_const (i₀ := 1) fun n hn ↦ by
-    simp [multiplesOf_eq_univ A h, Set.partialDensity]
-    lia
-
-@[category test, AMS 11]
-theorem isWeaklyBehrend_of_ge_one {ι : Type*} (A : ι → ℕ) {ε : ℝ} (hε : 1 ≤ ε) :
-    IsWeaklyBehrend A ε := by
-  exact (sub_nonpos.2 hε).trans (Set.lowerDensity_nonneg _)
-
-@[category test, AMS 11]
-theorem not_isWeaklyBehrend_of_neg {ι : Type*} (A : ι → ℕ) {ε : ℝ} (hε : ε < 0) :
-    ¬IsWeaklyBehrend A ε := by
-  norm_num [IsWeaklyBehrend]
-  exact (add_lt_of_neg_right _ hε).trans_le (Set.lowerDensity_le_one _)
+/-- A cube is the image of $\lbrace 0, 1\rbrace^n$ under a linear automorphism. -/
+def IsCube {p n : ℕ} [Fact p.Prime] (A : Set (𝔽 p n)) : Prop :=
+  ∃ φ : 𝔽 p n ≃ₗ[ZMod p] 𝔽 p n, A = φ '' StandardCube n
 
 /--
-Let $A\subset\mathbb{N}$ be infinite such that $\sum_{a \in A} \frac{1}{a} = \infty$. Must
-there exist some $k\geq 1$ such that almost all integers have a divisor of the form $a+k$
-for some $a\in A$?
-
-This was formalized in Lean by Alexeev using Aristotle.
+Let $A_1, \dots, A_{100}$ be "cubes" in $\mathbb{F}^n_3$.
+Is it true that $A_1 + \dots + A_{100} = \mathbb{F}^n_3$?
 -/
-@[category research formally solved using lean4 at
-"https://github.com/plby/lean-proofs/blob/main/src/v4.24.0/ErdosProblems/Erdos26.lean", AMS 11]
-theorem erdos_26 : answer(False) ↔ ∀ A : ℕ → ℕ, StrictMono A → IsThick A →
-    ∃ k, IsBehrend (A · + k) := by
+@[category research formally solved using formal_conjectures at "https://arxiv.org/abs/2510.01300", AMS 5 11 15]
+theorem green_26 :
+    ∀ n : ℕ,
+      ∀ A : Fin 100 → Set (𝔽₃ n), (∀ i, IsCube (A i)) →
+      ∑ i, A i = univ := by
   sorry
+
+/-- [Yu25] has solved the original problem (with 100 replaced by 4) -/
+@[category research formally solved using formal_conjectures at "https://arxiv.org/abs/2510.01300", AMS 5 11 15]
+theorem green_26.variants.yu25 :
+    ∀ n : ℕ,
+    ∀ A : Fin 4 → Set (𝔽₃ n), (∀ i, IsCube (A i)) →
+      ∑ i, A i = univ := by
+  sorry
+
+open Asymptotics Filter
 
 /--
-If we allow for $\sum_{a\in A} \frac{1}{a} < \infty$ then Rusza has found a counter-example.
+[ALM91] showed that if 100 is replaced by $\leq c(p) \log n$ then the result is true for
+$\mathbb{F}^n_p$.
 -/
-@[category research solved, AMS 11]
-theorem erdos_26.variants.rusza : ∃ A : ℕ → ℕ,
-    StrictMono A ∧ ¬IsThick A ∧ ∀ k, ¬IsBehrend (A · + k) := by
+@[category research formally solved using formal_conjectures at "https://doi.org/10.1016/0097-3165(91)90046-I", AMS 5 11 15]
+theorem green_26.variants.alm91 :
+    ∀ (p : ℕ) [Fact p.Prime],
+      ∃ (k : ℕ → ℕ),
+        ((fun n ↦ (k n : ℝ)) =O[atTop] fun n ↦ Real.log n) ∧
+        ∀ᶠ n in atTop,
+          ∀ A : Fin (k n) → Set (𝔽 p n), (∀ i, IsCube (A i)) →
+          ∑ i, A i = univ := by
   sorry
 
-/--
-Tenenbaum asked the weaker variant where for every $\epsilon>0$ there is
-some $k=k(\epsilon)$ such that at least $1-\epsilon$ density of all integers have a
-divisor of the form $a+k$ for some $a\in A$.
--/
-@[category research open, AMS 11]
-theorem erdos_26.variants.tenenbaum : answer(sorry) ↔ ∀ᵉ (A : ℕ → ℕ), StrictMono A → IsThick A →
-    (∀ ε > (0 : ℝ), ∃ k, IsWeaklyBehrend (A · + k) ε) := by
+/-- The analogous problem in $\mathbb{F}^n_p$ remains open. [Gr24] -/
+@[category research open, AMS 5 11 15]
+theorem green_26.variants.open :
+    answer(sorry) ↔ ∀ (p : ℕ) [Fact p.Prime],
+      (∃ C, ∀ n, ∀ A : Fin C → Set (𝔽 p n), (∀ i, IsCube (A i)) →
+      ∑ i, A i = univ) := by
   sorry
 
-end Erdos26
+end Green26
