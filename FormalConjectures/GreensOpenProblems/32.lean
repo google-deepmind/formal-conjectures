a75f76044337a60d4c675f2492227255577e9646
Jean-Guillaume Durand <jeangud7@gmail.com>
2026-03-10 09:59:48 -0700

    Added sh20_general

--- FormalConjectures/GreensOpenProblems/32.lean
+++ FormalConjectures/GreensOpenProblems/32.lean
@@ -56,6 +56,14 @@ theorem green_32 :
     answer(sorry) ↔ HasLargeGapDilate (fun p ↦ Real.sqrt p) := by
   sorry
 
+/-- [Sh20, Theorem 1] implies a gap of at least $\lceil 2p/|A| - 2 \rceil$. -/
+@[category research solved, AMS 5 11]
+theorem green_32.variants.sh20_general :
+    ∀ᶠ p in atTop, p.Prime →
+      ∀ A : Finset (ZMod p), 1 < A.card →
+      ∃ c : (ZMod p)ˣ, HasGap (c • A) ⌈2 * (p : ℝ) / A.card - 2⌉₊ := by
+  sorry
+
 /--
 [Sh20] has used the polynomial method to show that this is true with 100 replaced by 2 [Gr24].
 
@@ -64,7 +72,7 @@ For a set $A$ of size $\lfloor \sqrt{p} \rfloor$, this guarantees a gap of at le
 $\lfloor 2\sqrt{p} \rfloor - 2$.
 -/
 @[category research solved, AMS 5 11]
-theorem green_32.variants.sh20 :
+theorem green_32.variants.sh20_sqrt :
     ∀ᶠ p in atTop, p.Prime →
       ∀ A : Finset (ZMod p), A.card = Nat.sqrt p →
       ∃ c : (ZMod p)ˣ, HasGap (c • A) (⌊2 * Real.sqrt (p : ℝ)⌋₊ - 2) := by
