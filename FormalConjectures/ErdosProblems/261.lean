13bc807fc55e9be79cf58f8ddcfdfc52e5c9483d
Rmal <97214596+CoolRmal@users.noreply.github.com>
2026-04-21 18:26:13 -0400

    Update 261.lean

--- FormalConjectures/ErdosProblems/261.lean
+++ FormalConjectures/ErdosProblems/261.lean
@@ -31,45 +31,45 @@ open scoped Cardinal
 
 namespace Erdos261
 
-/-- An natural number `n` is said to have property `Erdos261_prop` if there exists `t ≥ 2` and
-distinct integers `a₀, a₁, ..., aₜ ≥ 1` such that `n / 2 ^ n = ∑ k, aₖ / 2 ^ aₖ`. -/
+/-- A natural number $n$ is said to have property `Erdos261_prop` if there exists $t \ge 2$ and
+distinct integers $a_0, a_1, \ldots, a_t \ge 1$ such that
+$n / 2^n = \sum_k a_k / 2^{a_k}$. -/
 def Erdos261_prop (n : ℕ) : Prop := ∃ᵉ (t ≥ 2) (a : Fin t → ℕ), a.Injective ∧
   (1 ≤ a) ∧ n / (2 ^ n : ℚ) = ∑ k, (a k) / (2 ^ (a k) : ℚ)
 
 /-- Borwein and Loring used the following example in [BoLO90] to show that there are infinitely
-many natural numbers `n` that satisfy `Erdos261_prop`. -/
+many natural numbers $n$ that satisfy `Erdos261_prop`. -/
 @[category high_school, AMS 11]
 theorem erdos_261.example (m : ℕ) :
     letI n := 2 ^ (m + 1) - m - 2
     n / (2 ^ n : ℚ) = ∑ k ∈ Finset.Ioc n (n + m), k / (2 ^ k : ℚ) := by
   sorry
 
-/-- As a corollary, there exists infinitely many numbers with the property.
-#TODO: use `erdos_261.example` to prove the following theorem. -/
+/-- As a corollary, there exist infinitely many numbers with the property. -/
 @[category research solved, AMS 11]
 theorem erdos_261.infinite : {n : ℕ | Erdos261_prop n}.Infinite := by
   sorry
 
-/-- It is verified in [TUZ20] that all `n ≤ 10000` satisfy `Erdos261_prop`. -/
+/-- It is verified in [TUZ20] that all $n \le 10000$ satisfy `Erdos261_prop`. -/
 @[category research solved, AMS 11]
 theorem erdos_261.le_10000 {n : ℕ} (hn : n ≤ 10000) : Erdos261_prop n := by
   sorry
 
-/-- Do all `n` satisfy `Erdos261_prop`? -/
+/-- Do all $n$ satisfy `Erdos261_prop`? -/
 @[category research open, AMS 11]
 theorem erdos_261.all : answer(sorry) ↔ ∀ n, Erdos261_prop n := by
   sorry
 
-/-- Does there exists a rational `x` such that `x = ∑' k, (a k) / 2 ^ (a k)` has at least `𝔠`
-many solutions? -/
+/-- Does there exist a rational `x` such that $x = \sum'_k (a(k)) / 2^{a(k)}$ has at least
+$\mathfrak{c}$ many solutions? -/
 @[category research open, AMS 11]
 theorem erdos_261.rational : answer(sorry) ↔ ∃ x : ℚ,
     𝔠 ≤ #{a : ℕ → ℕ | Summable (fun k => (a k) / (2 ^ (a k) : ℚ)) ∧
     x = ∑' k, (a k) / (2 ^ (a k) : ℚ)} := by
   sorry
 
-/-- Does there exists a rational `x` such that `x = ∑' k, (a k) / 2 ^ (a k)` has at least `2`
-solutions? -/
+/-- Does there exist a rational `x` such that $x = \sum'_k (a(k)) / 2^{a(k)}$ has at least
+$2$ solutions? -/
 @[category research open, AMS 11]
 theorem erdos_261.rational.weak : answer(sorry) ↔ ∃ x : ℚ,
     2 ≤ #{a : ℕ → ℕ | Summable (fun k => (a k) / (2 ^ (a k) : ℚ)) ∧
