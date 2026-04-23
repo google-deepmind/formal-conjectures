a3678aa90ed10ae2d4c51b01aeb2112823d9499d
Jean-Guillaume Durand <jeangud7@gmail.com>
2026-04-22 20:45:37 -0700

    Added intermediate theorems

--- FormalConjectures/GreensOpenProblems/40.lean
+++ FormalConjectures/GreensOpenProblems/40.lean
@@ -29,37 +29,67 @@ open scoped Pointwise
 
 namespace Green40
 
-def hammingBall (n r : ℕ) : Set (Fin n → ZMod 2) :=
+/-- The vector space $\mathbb{F}_2^n$. -/
+abbrev 𝔽₂ (n : ℕ) := Fin n → ZMod 2
+
+/-- The Hamming ball of radius $r$ in $\mathbb{F}_2^n$. -/
+def hammingBall (n r : ℕ) : Set (𝔽₂ n) :=
   {x | (Finset.univ.filter (fun i => x i ≠ 0)).card ≤ r}
 
-def isCoveringSubspace (n r : ℕ) (V : Submodule (ZMod 2) (Fin n → ZMod 2)) : Prop :=
-  (V : Set (Fin n → ZMod 2)) + hammingBall n r = Set.univ
+/-- $V$ is a covering subspace of $\mathbb{F}_2^n$ by $H(r)$ if $V + H(r) = \mathbb{F}_2^n$. -/
+def isCoveringSubspace (n r : ℕ) (V : Submodule (ZMod 2) (𝔽₂ n)) : Prop :=
+  (V : Set (𝔽₂ n)) + hammingBall n r = Set.univ
 
+/-- There exists an infinite sequence of $n$'s together with subspaces $V_n \leq \mathbb{F}_2^n$
+with $V_n + H(r) = \mathbb{F}_2^n$ and $|V_n| = \left(f(r) + o(1)\right) \frac{2^n}{|H(r)|}$. -/
 def sequence_exists (r : ℕ) (c : ℝ) : Prop :=
-  ∀ ε > 0, ∃ᶠ n in atTop, ∃ V : Submodule (ZMod 2) (Fin n → ZMod 2),
+  ∀ ε > 0, ∃ᶠ n in atTop, ∃ V : Submodule (ZMod 2) (𝔽₂ n),
     isCoveringSubspace n r V ∧
     |(Nat.card V : ℝ) * (Nat.card (hammingBall n r) : ℝ) / (2 ^ n : ℝ) - c| < ε
 
+/--
+Let $f(r)$ be the smallest constant such that there exists an infinite sequence of $n$'s together
+with subspaces $V_n \leq \mathbb{F}_2^n$ with $V_n + H(r) = \mathbb{F}_2^n$ and
+$|V_n| = \left(f(r) + o(1)\right) \frac{2^n}{|H(r)|}$.
+-/
 noncomputable def f (r : ℕ) : ℝ := sInf {c : ℝ | sequence_exists r c}
 
-/-- Does $f(r) \to \infty$? -/
+/-- Does $f(r) \to \infty$? [Gr24]-/
 @[category research open, AMS 5 94]
 theorem green_40 : answer(sorry) ↔ Tendsto f atTop atTop := by
   sorry
 
-def isCoveringFinset (n r : ℕ) (V : Finset (Fin n → ZMod 2)) : Prop :=
-  (V : Set (Fin n → ZMod 2)) + hammingBall n r = Set.univ
+/-- The only value known is $f(1) = 1$, which follows from the existence of the Hamming code [Gr24]. -/
+@[category research solved, AMS 5 94]
+theorem green_40.sanity_f_one : f 1 = 1 := by
+  sorry
+
+-- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-- Variant with arbitrary subsets
+-- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+def isCoveringFinset (n r : ℕ) (V : Finset (𝔽₂ n)) : Prop :=
+  (V : Set (𝔽₂ n)) + hammingBall n r = Set.univ
 
 def subset_sequence_exists (r : ℕ) (c : ℝ) : Prop :=
-  ∀ ε > 0, ∃ᶠ n in atTop, ∃ V : Finset (Fin n → ZMod 2),
+  ∀ ε > 0, ∃ᶠ n in atTop, ∃ V : Finset (𝔽₂ n),
     isCoveringFinset n r V ∧
     |(V.card : ℝ) * (Nat.card (hammingBall n r) : ℝ) / (2 ^ n : ℝ) - c| < ε
 
 noncomputable def f_tilde (r : ℕ) : ℝ := sInf {c : ℝ | subset_sequence_exists r c}
 
-/-- Does $\tilde{f}(r) \to \infty$? -/
+/-- Does $\tilde{f}(r) \to \infty$? [Gr24] -/
 @[category research open, AMS 5 94]
 theorem green_40.variants.arbitrary_subsets : answer(sorry) ↔ Tendsto f_tilde atTop atTop := by
+  sorry
+
+/-- It is known that $\tilde{f}(2) = 1$ [Gr24]. -/
+@[category research solved, AMS 5 94]
+theorem green_40.variants.arbitrary_subsets_sanity_f_tilde_two : f_tilde 2 = 1 := by
+  sorry
+
+/-- We evidently have $\tilde{f}(r) \le f(r)$ [Gr24]. -/
+@[category research solved, AMS 5 94]
+theorem green_40.f_tilde_le_f (r : ℕ) : f_tilde r ≤ f r := by
   sorry
 
 end Green40
