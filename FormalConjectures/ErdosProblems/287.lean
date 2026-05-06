c9a6fb9474bbf94a893460b07d4551823ed7702f
Viktorija Brikule <viktoriabrikule@gmail.com>
2026-05-06 20:35:30 +0100

    Update 287.lean
    Rewrote answer(sorry)

--- FormalConjectures/ErdosProblems/287.lean
+++ FormalConjectures/ErdosProblems/287.lean
@@ -34,8 +34,10 @@ $1 < n_1 < \cdots < n_k$ such that $\sum_{i=1}^k \frac{1}{n_i} = 1$,
 we must have $\max(n_{i+1} - n_i) \geq 3$?
 -/
 @[category research open, AMS 11]
-theorem erdos_287 (k : ℕ) (hk : 2 ≤ k) (s : Fin k → ℕ) (smono : StrictMono s) (s1 : 1 < s ⟨0, by omega⟩) :
-    answer(sorry) ↔ ∑ i : Fin k, 1/ (s i : ℕ) = 1 →
+theorem erdos_287 : answer(sorry) ↔
+    ∀ (k : ℕ) (hk : 2 ≤ k) (s : Fin k → ℕ),
+    StrictMono s → 1 < s ⟨0, by omega⟩ →
+    ∑ i : Fin k, 1/ (s i : ℝ) = 1 →
     3 ≤ max_gap k s:= by
   sorry
 
