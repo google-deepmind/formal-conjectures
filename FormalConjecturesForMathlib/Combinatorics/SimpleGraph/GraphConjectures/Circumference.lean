51b313e29a7369cf4ed8dfb1f4fee32101908f1f
Daniel Ilkovic <danieli@google.com>
2026-05-20 10:42:56 +0000

    split into separate files

---
+++ FormalConjecturesForMathlib/Combinatorics/SimpleGraph/GraphConjectures/Circumference.lean
@@ -0,0 +1,30 @@
+/-
+Copyright 2025 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+module
+public import FormalConjecturesForMathlib.Combinatorics.SimpleGraph.GraphConjectures.Definitions
+
+@[expose] public section
+
+namespace SimpleGraph
+open Classical
+
+variable {α : Type*} [Fintype α] [DecidableEq α]
+-- Longest cycle length in `G`, or `0` if `G` is acyclic. -/
+noncomputable def circumference (G : SimpleGraph α) : ℕ :=
+  sSup { n | ∃ (a : α) (w : G.Walk a a), w.IsCycle ∧ w.length = n }
+
+
+  end SimpleGraph
