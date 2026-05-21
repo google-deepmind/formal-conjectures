2965986b60ca3a8a6747d96a5b6b78df596bb716
Paul Lezeau <paul.lezeau@gmail.com>
2026-05-21 18:25:25 +0100

    Extract group growth API to ForMathlib

--- FormalConjectures/Wikipedia/GapConjecture.lean
+++ FormalConjectures/Wikipedia/GapConjecture.lean
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -/
 
-import FormalConjectures.Wikipedia.GromovPolynomialGrowth
+import FormalConjectures.Util.ProblemImports
 
 /-!
 # Gap conjecture
