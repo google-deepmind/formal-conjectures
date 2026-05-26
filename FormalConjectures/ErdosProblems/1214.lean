6498967b3539d9082411e69d3895b33256be12e8
danielchin <danielchin93@gmail.com>
2026-05-25 22:34:00 -0700

    fix review comments

--- FormalConjectures/ErdosProblems/1214.lean
+++ FormalConjectures/ErdosProblems/1214.lean
@@ -21,15 +21,15 @@ import FormalConjectures.Util.ProblemImports
 
 *References:*
 - [erdosproblems.com/1214](https://www.erdosproblems.com/1214)
-- [CoSc97] [CoSc97] Corrales-Rodrigáñez, Capi and Schoof, René, The support problem and its
-  elliptic analogue. J. Number Theory (1997), 276--290.
+- [CoSc97] Corrales-Rodrigáñez, Capi and Schoof, René, The support problem and its
+  elliptic analogue. J. Number Theory (1997) [Volume 64, Issue 2], 276--290.
 -/
 
 namespace Erdos1214
 
 /--
 Let $x,y\geq 1$ be integers such that, for all $n\geq 1$, the set of primes dividing $x^{n}-1$ is
-equal to set of primes dividing $y^n-1$. Must $x=y$?
+equal to the set of primes dividing $y^n-1$. Must $x=y$?
 
 Erdős asked this at a 1988 number theory conference in Banff.
 
