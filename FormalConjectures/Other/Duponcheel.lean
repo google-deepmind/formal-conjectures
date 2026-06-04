73e5fde79fb570792ce246b8d5b823e8c4915de9
Luc Duponcheel <38331763+LucDuponcheelAtGitHub@users.noreply.github.com>
2026-06-04 21:53:31 +0200

    Refactor n_c_coins_placement_conjecture theorem
    I changed the code a bit, but, more important, I added docstrings in order to be compliant with the style guidelines

--- FormalConjectures/Other/Duponcheel.lean
+++ FormalConjectures/Other/Duponcheel.lean
@@ -14,8 +14,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -/
 
+
 import Mathlib.Data.Matrix.Basic
 import Mathlib.Data.Fin.Basic
+
+import FormalConjectures.Util.ProblemImports
 
 /-!
 # Duponcheel's Generalized n-Queens Conjecture
@@ -31,35 +34,39 @@ This generalizes the traditional n-queens problem
 (which corresponds to `c = 1`).
 -/
 
+namespace DuponcheelConjecture
+
+variable {n : ℕ} (hn : n ≥ 4) {c : ℕ} (hc : c ≤ n)
+
 /--
 Coins on an `n × n` board, represented as a boolean matrix.
   `true`  indicates a  coin being present
   `false` indicates no coin being present
 -/
-
-import Mathlib.Data.Matrix.Basic
-import Mathlib.Data.Fin.Basic
-
-namespace DuponcheelConjecture
-
-variable {n : ℕ} (hn : n ≥ 4) {c : ℕ} (hc : c ≤ n)
-
-abbrev BoolBoard (n : ℕ) := Matrix (Fin n) (Fin n) Bool
-
-abbrev NatBoard (n : ℕ) := Matrix (Fin n) (Fin n) ℕ
-
-theorem n_c_coins_placement_conjecture :
-    ∃ (B : BoolBoard n),
-      let N := B.map (fun b ↦ if b then 1 else 0) ;
-      -- Each row, column, upward diagonal, and downward diagonal
-      -- contains at most `c` coins.
-      ∀ i : Fin n, ∑ j : Fin n, N i j ≤ c ∧
-      ∀ j : Fin n, ∑ i : Fin n, N i j ≤ c ∧
-      ∀ k : ℕ, ∑ i : Fin n, ∑ j : Fin n, (if i + j = k then N i j else 0) ≤ c ∧
-      ∀ k : ℤ, ∑ i : Fin n, ∑ j : Fin n, (if i - j = k then N i j else 0) ≤ c ∧
-      -- Total number of coins placed is `n * c`.
-      ∑ i : Fin n, ∑ j : Fin n, N i j = n * c
-
+@[category research open, AMS 5]
+theorem n_c_coins_placement_conjecture : answer(sorry) ↔
+    ∃ (BoolBoard : Matrix (Fin n) (Fin n) Bool),
+      let NatBoard : Matrix (Fin n) (Fin n) ℕ :=
+        BoolBoard.map (fun b ↦ if b then 1 else 0)
+      let rc (i : Fin n) :=
+        ∑ j : Fin n, NatBoard i j
+      let cc (i : Fin n) :=
+        ∑ j : Fin n, NatBoard j i
+      let ddc (k : ℕ) :=
+        ∑ i : Fin n, ∑ j : Fin n, (if i + j = k then NatBoard i j else 0)
+      let udc (k : ℤ) :=
+        ∑ i : Fin n, ∑ j : Fin n, (if i - j = k then NatBoard i j else 0)
+      let bc :=
+        ∑ i : Fin n, ∑ j : Fin n, NatBoard i j
+      -- Each row count and column count
+      -- is less than or equal to `c`
+      ∀ i : Fin n, rc i ≤ c ∧ cc i ≤ c ∧
+      -- Each downward diagonal count and upward diagonal count
+      -- is less than or equal to `c`
+      ∀ k : ℕ, ddc k ≤ c ∧ ∀ k : ℤ, udc k ≤ c ∧
+      -- The board count
+      -- is equal to `n * c`.
+      bc = n * c
   := by sorry
 
 
