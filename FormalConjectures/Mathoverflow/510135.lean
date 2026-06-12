59d01e1824e7b96900057703840fca71f9c6af3c
Luc Duponcheel <38331763+LucDuponcheelAtGitHub@users.noreply.github.com>
2026-06-13 01:36:08 +0200

    Fix reference and user details in 510135.lean
    Moved renamed version to Mathoverflow subdirectory

---
+++ FormalConjectures/Mathoverflow/510135.lean
@@ -0,0 +1,58 @@
+/-
+
+import FormalConjectures.Util.ProblemImports
+
+/-!
+# Duponcheel's Generalized n-Queens Conjecture
+# (replacing the traditional n-queens problem with a coin placement problem)
+
+*References:*
+- [mathoverflow/513435](https://mathoverflow.net/questions/510135)
+  asked by user [*Luc Duponcheel*](https://mathoverflow.net/users/513435/lucdupatstackexchange)
+
+Given `n ≥ 4` and `0 ≤ c ≤ n`,
+it is always possible to place `n * c` coins on an `n × n` board such that
+no row, column, upward diagonal, or downward diagonal
+contains more than `c` coins.
+Each cell can hold at most 1 coin.
+
+This generalizes the traditional n-queens problem
+(which corresponds to `c = 1`).
+
+Coins on an `n × n` board, represented as a boolean matrix.
+  `true`  indicates a  coin being present
+  `false` indicates no coin being present
+-/
+
+namespace Mathoverflow513435
+
+variable {n : ℕ} (hn : n ≥ 4) {c : ℕ} (hc : c ≤ n)
+
+@[category research open, AMS 5]
+theorem n_c_coins_placement_conjecture : answer(sorry) ↔
+    ∃ (board : Matrix (Fin n) (Fin n) ℕ),
+      let rc (i : Fin n) :=
+        ∑ j : Fin n, board i j
+      let cc (i : Fin n) :=
+        ∑ j : Fin n, board j i
+      let ddc (k : ℕ) :=
+        ∑ i : Fin n, ∑ j : Fin n, (if i + j = k then board i j else 0)
+      let udc (k : ℤ) :=
+        ∑ i : Fin n, ∑ j : Fin n, (if i - j = k then board i j else 0)
+      let bc :=
+        ∑ i : Fin n, ∑ j : Fin n, board i j
+      -- Only zero or one coins at a position are allowed
+      ∀ i : Fin n, ∀ j : Fin n, 0 ≤ board i j ∧ board i j ≤ 1 ∧
+      -- Each row count is less than or equal to `c`
+      ∀ i : Fin n, rc i ≤ c ∧
+      -- Each column count is less than or equal to `c`
+      ∀ i : Fin n, cc i ≤ c ∧
+      -- Each downward diagonal count is less than or equal to `c`
+      ∀ k : ℕ, ddc k ≤ c ∧
+      -- Each upward diagonal count is less than or equal to `c`
+      ∀ k : ℤ, udc k ≤ c ∧
+      -- The board count is equal to `n * c`.
+      bc = n * c
+  := by sorry
+
+end Mathoverflow513435
