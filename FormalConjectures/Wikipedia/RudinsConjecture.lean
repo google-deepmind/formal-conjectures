d0d26237e0774c450d48588a92a7b79c4def85a5
Himanshu Gupta <him141195@gmail.com>
2026-07-21 09:49:46 -0700

    Update FormalConjectures/Wikipedia/RudinsConjecture.lean
    Co-authored-by: Moritz Firsching <firsching@google.com>

--- FormalConjectures/Wikipedia/RudinsConjecture.lean
+++ FormalConjectures/Wikipedia/RudinsConjecture.lean
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -/
 
-import FormalConjectures.Util.ProblemImports
+import FormalConjecturesUtil
 
 /-!
 # Rudin's conjecture on squares in arithmetic progressions
