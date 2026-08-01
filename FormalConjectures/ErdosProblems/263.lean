7f4337c76300d45111c88aa48f72a7b0b6edf952
arex1337 <alexander.lystad@gmail.com>
2026-08-01 10:09:03 +0200

    ErdosProblems/263: link formal proof of super_doubly_exponential

--- FormalConjectures/ErdosProblems/263.lean
+++ FormalConjectures/ErdosProblems/263.lean
@@ -91,7 +91,8 @@ On the other hand, if there exists some $\varepsilon > 0$ such that $a_n$ satisf
 $\liminf \frac{a_{n+1}}{a_n^{2+\varepsilon}} > 0$, then $a_n$ is an irrationality sequence
 by the above folklore result `erdos_263.variants.folklore`.
 -/
-@[category research solved, AMS 11]
+@[category research solved, AMS 11,
+  formal_proof using lean4 at "https://github.com/arex1337/erdos-263-lean/blob/95de79a/Erdos263/Folklore.lean"]
 theorem erdos_263.variants.super_doubly_exponential (a: ℕ -> ℕ)
     (ha : ∀ n : ℕ, a n > 0)
     (ha' : StrictMono a)
