48d85b48f1bdeeef1ac936d2289e3572abcb8fc9
Moritz Firsching <firsching@google.com>
2026-08-14 12:00:09 +0200

    Update FormalConjectures/Books/BugeaudDistributionModuloOne/Problem10_9.lean

--- FormalConjectures/Books/BugeaudDistributionModuloOne/Problem10_9.lean
+++ FormalConjectures/Books/BugeaudDistributionModuloOne/Problem10_9.lean
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -/
 
-import FormalConjectures.Util.ProblemImports
+import FormalConjecturesUtil
 import FormalConjectures.Wikipedia.Mahler32
 /-!
 # Bugeaud Collection of Conjectures and Open Questions: Mahler's Z-numbers
