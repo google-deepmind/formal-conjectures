c6d9126429cbcf0ae251191f932a6b7aa3e0276a
Paul Lezeau <paul.lezeau@gmail.com>
2026-08-20 16:52:02 +0100

    Use the ForMathlib umbrella in Caratheodory

--- FormalConjectures/Other/CaratheodoryConjecture.lean
+++ FormalConjectures/Other/CaratheodoryConjecture.lean
@@ -15,7 +15,6 @@ limitations under the License.
 -/
 
 import FormalConjecturesUtil
-import FormalConjecturesForMathlib.Geometry.EuclideanHypersurface
 import FormalConjectures.Other.LoewnerConjecture
 
 /-!
