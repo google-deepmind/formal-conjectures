32a4461d18897b29cfaa78ecd2c95595eb8f1bc8
Moritz Firsching <firsching@google.com>
2026-08-26 08:41:30 +0200

    Apply suggestion from @mo271

--- FormalConjectures/ErdosProblems/372.lean
+++ FormalConjectures/ErdosProblems/372.lean
@@ -14,8 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -/
 
-import FormalConjectures.Util.Attributes.Basic
-import FormalConjecturesForMathlib.Data.Nat.MaxPrimeFac
+import FormalConjecturesUtil
 
 /-!
 # Erdős Problem 372
