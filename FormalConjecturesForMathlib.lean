df232dc09150303695abd05039e3a24ee8d924ed
dawidmtrela-dotcom <324788959+dawidmtrela-dotcom@users.noreply.github.com>
2026-09-07 00:04:33 +0200

    feat: formalize the largest small polygon problem

--- FormalConjecturesForMathlib.lean
+++ FormalConjecturesForMathlib.lean
@@ -129,6 +129,7 @@ public import FormalConjecturesForMathlib.Data.ZMod.PerfectDifferenceSet
 public import FormalConjecturesForMathlib.FieldTheory.MvRatFunc.Defs
 public import FormalConjecturesForMathlib.Geometry.Euclidean
 public import FormalConjecturesForMathlib.Geometry.Metric
+public import FormalConjecturesForMathlib.Geometry.Polygon
 public import FormalConjecturesForMathlib.Geometry.«2d»
 public import FormalConjecturesForMathlib.Geometry.«3d»
 public import FormalConjecturesForMathlib.Lean.Elab.InfoTree.Util
