6b9031f3ea7ed094d797fcf0e68199e2d2de4e34
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-09-08 15:38:31 +0200

    fix

--- FormalConjectures/Arxiv/1810.00617/BassQuillen.lean
+++ FormalConjectures/Arxiv/1810.00617/BassQuillen.lean
@@ -33,9 +33,9 @@ open TensorProduct IsLocalRing
 /-- The `Bass Quillen Conjecture`, stating that any fg projective module over polynomial ring
 over regular ring `R` comes from a projective module over `R`. -/
 @[category research open, AMS 13]
-theorem BassQuillenConjecture [IsRegularRing R] (P : Type*) [AddCommGroup P] [Module R P]
+theorem BassQuillenConjecture [IsRegularRing R] (P : Type u) [AddCommGroup P] [Module R P]
     [Module (MvPolynomial ι R) P] [IsScalarTower R (MvPolynomial ι R) P]
-    [Module.Projective (MvPolynomial ι R) P] :
+    [Module.Finite (MvPolynomial ι R) P] [Module.Projective (MvPolynomial ι R) P] :
     ∃ (Q : Type u) (_ : AddCommGroup Q) (_ : Module R Q),
       Nonempty (((MvPolynomial ι R) ⊗[R] Q) ≃ₗ[MvPolynomial ι R] P) := by
   sorry
@@ -45,9 +45,9 @@ section lowdimesnion
 /-- The `Bass Quillen Conjecture` is true for dimension less than `2`. -/
 @[category research solved, AMS 13]
 theorem BassQuillenConjecture_of_dim_le_two [IsRegularRing R] (le2 : ringKrullDim R ≤ 2)
-    (P : Type*) [AddCommGroup P] [Module R P]
+    (P : Type u) [AddCommGroup P] [Module R P]
     [Module (MvPolynomial ι R) P] [IsScalarTower R (MvPolynomial ι R) P]
-    [Module.Projective (MvPolynomial ι R) P] :
+    [Module.Finite (MvPolynomial ι R) P] [Module.Projective (MvPolynomial ι R) P] :
     ∃ (Q : Type u) (_ : AddCommGroup Q) (_ : Module R Q),
       Nonempty (((MvPolynomial ι R) ⊗[R] Q) ≃ₗ[MvPolynomial ι R] P) := by
   sorry
@@ -57,9 +57,9 @@ theorem BassQuillenConjecture_of_dim_le_two [IsRegularRing R] (le2 : ringKrullDi
 @[category research solved, AMS 13]
 theorem BassQuillenConjecture_of_dim_eq_three [IsRegularLocalRing R] (le2 : ringKrullDim R = 3)
     (ne2 : ringChar (ResidueField R) ≠ 2) (ne3 : ringChar (ResidueField R) ≠ 3)
-    (P : Type*) [AddCommGroup P] [Module R P]
+    (P : Type u) [AddCommGroup P] [Module R P]
     [Module (MvPolynomial ι R) P] [IsScalarTower R (MvPolynomial ι R) P]
-    [Module.Projective (MvPolynomial ι R) P] :
+    [Module.Finite (MvPolynomial ι R) P] [Module.Projective (MvPolynomial ι R) P] :
     ∃ (Q : Type u) (_ : AddCommGroup Q) (_ : Module R Q),
       Nonempty (((MvPolynomial ι R) ⊗[R] Q) ≃ₗ[MvPolynomial ι R] P) := by
   sorry
@@ -69,9 +69,9 @@ end lowdimesnion
 /-- The `Bass Quillen Conjecture` is true for local ring of char zero. -/
 @[category research solved, AMS 13]
 theorem BassQuillenConjecture_of_char_zero [IsRegularLocalRing R] [CharZero (ResidueField R)]
-    (P : Type*) [AddCommGroup P] [Module R P]
+    (P : Type u) [AddCommGroup P] [Module R P]
     [Module (MvPolynomial ι R) P] [IsScalarTower R (MvPolynomial ι R) P]
-    [Module.Projective (MvPolynomial ι R) P] :
+    [Module.Finite (MvPolynomial ι R) P] [Module.Projective (MvPolynomial ι R) P] :
     ∃ (Q : Type u) (_ : AddCommGroup Q) (_ : Module R Q),
       Nonempty (((MvPolynomial ι R) ⊗[R] Q) ≃ₗ[MvPolynomial ι R] P) := by
   sorry
@@ -80,9 +80,9 @@ theorem BassQuillenConjecture_of_char_zero [IsRegularLocalRing R] [CharZero (Res
 @[category research solved, AMS 13]
 theorem BassQuillenConjecture_of_unramified [IsRegularLocalRing R]
     (unram : (ringChar (ResidueField R) : R) ∉ (maximalIdeal R) ^ 2)
-    (P : Type*) [AddCommGroup P] [Module R P]
+    (P : Type u) [AddCommGroup P] [Module R P]
     [Module (MvPolynomial ι R) P] [IsScalarTower R (MvPolynomial ι R) P]
-    [Module.Projective (MvPolynomial ι R) P] :
+    [Module.Finite (MvPolynomial ι R) P] [Module.Projective (MvPolynomial ι R) P] :
     ∃ (Q : Type u) (_ : AddCommGroup Q) (_ : Module R Q),
       Nonempty (((MvPolynomial ι R) ⊗[R] Q) ≃ₗ[MvPolynomial ι R] P) := by
   sorry
