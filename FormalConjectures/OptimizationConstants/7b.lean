a0049956f24c965ee700fa7987fd681db700e007
Batixx <s59fpern@uni-bonn.de>
2026-09-08 15:05:27 +0200

    import

--- FormalConjectures/OptimizationConstants/7b.lean
+++ FormalConjectures/OptimizationConstants/7b.lean
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -/
 import FormalConjectures.OptimizationConstants.«7a»
+import FormalConjecturesUtil
 
 /-!
 # Tao's Optimization Constant 7b / The Irrationality Measure of $\Gamma(1/4)$
