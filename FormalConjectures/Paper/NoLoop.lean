d869c3e78e73135f5ab052de1282562571b35714
Yannis Monbru <yannis.monbru@orange.fr>
2026-09-08 17:53:30 +0200

    add implication between conjectures

--- FormalConjectures/Paper/NoLoop.lean
+++ FormalConjectures/Paper/NoLoop.lean
@@ -24,31 +24,60 @@ TODO
 
 universe u v w
 
+namespace NoLoopsConjectures
+
 /-
 Let `R`be an artinian ring, `A` an algebra of finite type and `S`a finitely generated simple module over `A` so that Ext^1(S,A) ≠ 0-/
-variable {R : Type u} {A : Type v} [CommRing R] [IsArtinianRing R] [Ring A] [Algebra R A] [Module.Finite R A] {S : ModuleCat.{v} A} [Module.Finite A S.carrier] [Simple S] (Ext1NeZ : ¬ Subsingleton (Ext S (.of A A) 1))
+variable {R : Type u} {A : Type v} [CommRing R] [IsArtinianRing R] [Ring A] [Algebra R A] [Module.Finite R A] (S : ModuleCat.{v} A) [Module.Finite A S] [Simple S]
 
+abbrev Ext1NeZ := ¬ Subsingleton (Ext S (.of A A) 1)
 
-namespace NoLoopsConjectures
+variable (A) in
+abbrev noLoopStatement := ∀ n:ℕ , ∃ M : ModuleCat.{v} A, Module.Finite A M ∧ projectiveDimension M > n
+
 /--
 In this situation the global dimension (defined as the supremum of proectives dimensions of finitely generated modules over `A`) of `A`is infinite.
 -/
 @[category research open, AMS 16 18] /- Associative rings and algebras + Category theory; homological algebra -/
-theorem noLoop : ∀ n, ∃ M: ModuleCat A,∃ _: Module.Finite A M.carrier, projectiveDimension M > n := by
+theorem noLoop : noLoopStatement A := by
   sorry
+
+abbrev strongNoLoopStatement := projectiveDimension S = ⊤
 
 /--
 In this situation the projective dimension of `S`is infinte.
 -/
 @[category research open, AMS 16 18] /- Associative rings and algebras + Category theory; homological algebra -/
-theorem strongNoLoop : projectiveDimension S = ⊤ := by
+theorem strongNoLoop : strongNoLoopStatement S := by
   sorry
+
+abbrev veryStrongNoLoopStatement := ∀ i, ∃ n >i,¬ Subsingleton (Ext S (.of A A) n)
 
 /--
 In this situation there are an infinte number of integer `i` such that Ext^i(S,A) are vanishing.
 -/
 @[category research open, AMS 16 18] /- Associative rings and algebras + Category theory; homological algebra -/
-theorem veryStrongNoLoop : ∀ i, ∃ n >i,¬ Subsingleton (Ext S (.of A A) n)  := by
+theorem veryStrongNoLoop : veryStrongNoLoopStatement S:= by
   sorry
+
+@[category test, AMS 16 18]
+lemma vStrongImplyStrong: (∀ S: ModuleCat A, Module.Finite A S → Simple S → Ext1NeZ S → veryStrongNoLoopStatement S ) → (∀ S: ModuleCat A, Module.Finite A S → Simple S → Ext1NeZ S → strongNoLoopStatement S ) := by
+  intro h S fS sS neZS
+  apply ENat.WithBot.eq_top_iff_forall_ge.mpr
+  intro m
+  apply le_sInf
+  rintro b hb
+  by_contra!
+  rcases h S fS sS neZS m with ⟨n,hn⟩
+  exact  hn.2 <| HasProjectiveDimensionLT.subsingleton
+    (hX := (hb _ this)) _ _ _ (le_of_lt hn.1 ) _
+
+@[category test, AMS 16 18]
+lemma StrongImplyNormal: (∀ S: ModuleCat A, Module.Finite A S → Simple S → Ext1NeZ S → strongNoLoopStatement S ) → (∀ S: ModuleCat A, Module.Finite A S → Simple S → Ext1NeZ S → noLoopStatement A) := by
+  intro h S fS sS neZS n
+  use S
+  refine ⟨fS,?_⟩
+  rw [ h S fS sS neZS]
+  apply WithBot.LT.coe_lt_coe <| ENat.natCast_lt_top n
 
 end NoLoopsConjectures
