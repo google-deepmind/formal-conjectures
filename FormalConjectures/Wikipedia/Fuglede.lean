94a3a978a74549339321b3dec97fef53ab821a00
Salvatore Mercuri <salvatore.mercuri@imperial.ac.uk>
2026-09-13 11:44:26 +0100

    docstring: use LaTeX for math in dim_3_or_higher

--- FormalConjectures/Wikipedia/Fuglede.lean
+++ FormalConjectures/Wikipedia/Fuglede.lean
@@ -52,10 +52,11 @@ theorem FugledeConjecture.variants.dim_2 :
   sorry
 
 /--
-**Fuglede's conjecture** fails in every dimension `n ≥ 3`: for each such `n` there is a bounded
-subset of ℝ^n with positive Lebesgue measure that is spectral but does not tile ℝ^n by translation,
-or that tiles ℝ^n by translation but is not spectral.
-(Note that counterexamples in lower dimensions would also disprove the conjecture in higher dimensions.)
+**Fuglede's conjecture** fails in every dimension $n \geq 3$: for each such $n$ there is a bounded
+subset of $\mathbb{R}^n$ with positive Lebesgue measure that is spectral but does not tile
+$\mathbb{R}^n$ by translation, or that tiles $\mathbb{R}^n$ by translation but is not spectral.
+(Note that counterexamples in lower dimensions would also disprove the conjecture in higher
+dimensions.)
 -/
 @[category research solved, AMS 42 46 47]
 theorem FugledeConjecture.variants.dim_3_or_higher (n : ℕ) (hn : 3 ≤ n) :
